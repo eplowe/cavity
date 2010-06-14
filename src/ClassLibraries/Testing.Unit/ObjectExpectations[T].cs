e186a9f6774bf087506a73476c68efedc9323257
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2010-06-14 20:40:48 +0000

    Extended ObjectExpectations<T> to allow the addition of custom expectations.

--- src/ClassLibraries/Testing.Unit/ObjectExpectations[T].cs
+++ src/ClassLibraries/Testing.Unit/ObjectExpectations[T].cs
@@ -33,55 +33,66 @@
 
         public ITestObject IsInterface()
         {
-            this.Items.Add(new InterfaceTest<T>());
+            (this as ITestObject).Add(new InterfaceTest<T>());
             return this;
         }
 
         public ITestObject IsValueType()
         {
-            this.Items.Add(new ValueTypeTest<T>());
+            (this as ITestObject).Add(new ValueTypeTest<T>());
             return this;
         }
 
         [SuppressMessage("Microsoft.Design", "CA1004:GenericMethodsShouldProvideTypeParameter", Justification = "Inference brings no benefit here.")]
         public ITestObjectStyle DerivesFrom<TBase>()
         {
-            this.Items.Add(new BaseClassTest<T>(typeof(TBase)));
+            (this as ITestObject).Add(new BaseClassTest<T>(typeof(TBase)));
             return this;
         }
 
         ITestObject ITestObjectStyle.IsAbstractBaseClass()
         {
-            this.Items.Add(new AbstractBaseClassTest<T>());
+            (this as ITestObject).Add(new AbstractBaseClassTest<T>());
             return this;
         }
 
         ITestObjectSealed ITestObjectStyle.IsConcreteClass()
         {
-            this.Items.Add(new ConcreteClassTest<T>());
+            (this as ITestObject).Add(new ConcreteClassTest<T>());
             return this;
         }
 
         ITestObjectConstruction ITestObjectSealed.IsSealed()
         {
-            this.Items.Add(new SealedClassTest<T>(true));
+            (this as ITestObject).Add(new SealedClassTest<T>(true));
             return this;
         }
 
         ITestObjectConstruction ITestObjectSealed.IsUnsealed()
         {
-            this.Items.Add(new SealedClassTest<T>(false));
+            (this as ITestObject).Add(new SealedClassTest<T>(false));
             return this;
         }
 
         ITestObject ITestObjectConstruction.HasDefaultConstructor()
         {
-            this.Items.Add(new DefaultConstructorTest<T>());
+            (this as ITestObject).Add(new DefaultConstructorTest<T>());
             return this;
         }
 
         ITestObject ITestObjectConstruction.NoDefaultConstructor()
         {
+            return this;
+        }
+
+        ITestObject ITestObject.Add(ITestExpectation expectation)
+        {
+            if (null == expectation)
+            {
+                throw new ArgumentNullException("expectation");
+            }
+
+            this.Items.Add(expectation);
             return this;
         }
 
@@ -93,7 +104,7 @@
                 throw new TestException(Resources.ObjectExpectationsException_InterfaceMessage);
             }
 
-            this.Items.Add(new ImplementationTest<T>(typeof(TInterface)));
+            (this as ITestObject).Add(new ImplementationTest<T>(typeof(TInterface)));
             return this;
         }
 
@@ -109,32 +120,32 @@
                 throw new TestException(Resources.ObjectExpectations_IsDecoratedWithXmlRoot);
             }
 
-            this.Items.Add(new AttributeMemberTest(typeof(T), typeof(TAttribute)));
+            (this as ITestObject).Add(new AttributeMemberTest(typeof(T), typeof(TAttribute)));
             return this;
         }
 
         ITestObject ITestObject.Serializable()
         {
-            this.Items.Add(new AttributeMemberTest(typeof(T), typeof(SerializableAttribute)));
-            this.Items.Add(new ImplementationTest<T>(typeof(ISerializable)));
+            (this as ITestObject).Add(new AttributeMemberTest(typeof(T), typeof(SerializableAttribute)));
+            (this as ITestObject).Add(new ImplementationTest<T>(typeof(ISerializable)));
             return this;
         }
 
         ITestObject ITestObject.XmlRoot(string name)
         {
-            this.Items.Add(new XmlRootTest<T>(name));
+            (this as ITestObject).Add(new XmlRootTest<T>(name));
             return this;
         }
 
         ITestObject ITestObject.XmlRoot(string name, string @namespace)
         {
-            this.Items.Add(new XmlRootTest<T>(name, @namespace));
+            (this as ITestObject).Add(new XmlRootTest<T>(name, @namespace));
             return this;
         }
 
         ITestObject ITestObject.IsNotDecorated()
         {
-            this.Items.Add(new AttributeMemberTest(typeof(T), null as Type));
+            (this as ITestObject).Add(new AttributeMemberTest(typeof(T), null as Type));
             return this;
         }
     }
