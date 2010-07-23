d29487e23bea2dc4d69eb70ecb28db5b92b0d7db
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2010-07-23 20:08:04 +0000

    Added CsvStreamReader.

---
+++ src/ClassLibraries/Core.Facts/IO/CsvStreamReader.Facts.cs
@@ -0,0 +1,154 @@
+﻿namespace Cavity.IO
+{
+    using System;
+    using System.IO;
+    using Xunit;
+
+    public sealed class CsvStreamReaderFacts
+    {
+        [Fact]
+        public void a_definition()
+        {
+            Assert.True(new TypeExpectations<CsvStreamReader>()
+                            .DerivesFrom<StreamReader>()
+                            .IsConcreteClass()
+                            .IsSealed()
+                            .NoDefaultConstructor()
+                            .IsNotDecorated()
+                            .Result);
+        }
+
+        [Fact]
+        public void ctor()
+        {
+            using (var stream = new MemoryStream())
+            {
+                Assert.NotNull(new CsvStreamReader(stream));
+            }
+        }
+
+        [Fact]
+        public void op_ReadEntry()
+        {
+            using (var stream = new MemoryStream())
+            {
+                using (var writer = new StreamWriter(stream))
+                {
+                    writer.WriteLine("A,B");
+                    writer.WriteLine("1A,1B");
+                    writer.Flush();
+                    stream.Position = 0;
+                    using (var reader = new CsvStreamReader(stream))
+                    {
+                        var actual = reader.ReadEntry();
+                        Assert.Equal(2, reader.LineNumber);
+                        Assert.Equal(1, reader.EntryNumber);
+                        Assert.Equal("1A", actual["A"]);
+                        Assert.Equal("1B", actual["B"]);
+                    }
+                }
+            }
+        }
+
+        [Fact]
+        public void op_ReadEntry_whenEmbeddedComma()
+        {
+            using (var stream = new MemoryStream())
+            {
+                using (var writer = new StreamWriter(stream))
+                {
+                    writer.WriteLine("A,B");
+                    writer.WriteLine("\"1,A\",\"1,B\"");
+                    writer.Flush();
+                    stream.Position = 0;
+                    using (var reader = new CsvStreamReader(stream))
+                    {
+                        var actual = reader.ReadEntry();
+                        Assert.Equal(2, reader.LineNumber);
+                        Assert.Equal(1, reader.EntryNumber);
+                        Assert.Equal("1,A", actual["A"]);
+                        Assert.Equal("1,B", actual["B"]);
+                    }
+                }
+            }
+        }
+
+        [Fact]
+        public void op_ReadEntry_whenMissingColumnItem()
+        {
+            using (var stream = new MemoryStream())
+            {
+                using (var writer = new StreamWriter(stream))
+                {
+                    writer.WriteLine("A,B,C");
+                    writer.WriteLine("1,2");
+                    writer.Flush();
+                    stream.Position = 0;
+                    using (var reader = new CsvStreamReader(stream))
+                    {
+                        Assert.Throws<FormatException>(() => reader.ReadEntry());
+                    }
+                }
+            }
+        }
+
+        [Fact]
+        public void op_ReadEntry_whenQuotation()
+        {
+            using (var stream = new MemoryStream())
+            {
+                using (var writer = new StreamWriter(stream))
+                {
+                    writer.WriteLine("A,B");
+                    writer.WriteLine("\"1A\",\"1B\"");
+                    writer.Flush();
+                    stream.Position = 0;
+                    using (var reader = new CsvStreamReader(stream))
+                    {
+                        var actual = reader.ReadEntry();
+                        Assert.Equal(2, reader.LineNumber);
+                        Assert.Equal(1, reader.EntryNumber);
+                        Assert.Equal("1A", actual["A"]);
+                        Assert.Equal("1B", actual["B"]);
+                    }
+                }
+            }
+        }
+
+        [Fact]
+        public void prop_EntryNumber()
+        {
+            Assert.NotNull(new PropertyExpectations<CsvStreamReader>("EntryNumber")
+                               .TypeIs<int>()
+                               .IsNotDecorated()
+                               .Result);
+        }
+
+        [Fact]
+        public void prop_Header()
+        {
+            Assert.NotNull(new PropertyExpectations<CsvStreamReader>("Header")
+                               .TypeIs<string>()
+                               .IsNotDecorated()
+                               .Result);
+        }
+
+        [Fact]
+        public void prop_Line()
+        {
+            Assert.NotNull(new PropertyExpectations<CsvStreamReader>("Line")
+                               .TypeIs<string>()
+                               .IsNotDecorated()
+                               .Result);
+        }
+
+        [Fact]
+        public void prop_LineNumber()
+        {
+            Assert.NotNull(new PropertyExpectations<CsvStreamReader>("LineNumber")
+                               .TypeIs<int>()
+                               .IsNotDecorated()
+                               .Result);
+        }
+    }
+}
---
+++ src/ClassLibraries/Core/IO/CsvStreamReader.cs
@@ -0,0 +1,119 @@
+﻿namespace Cavity.IO
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.IO;
+    using System.Text;
+    using Cavity.Properties;
+
+    public sealed class CsvStreamReader : StreamReader
+    {
+        public CsvStreamReader(Stream stream)
+            : base(stream)
+        {
+        }
+
+        public int EntryNumber { get; private set; }
+
+        public string Header { get; private set; }
+
+        public string Line { get; private set; }
+
+        public int LineNumber { get; private set; }
+
+        private List<string> Headings { get; set; }
+
+        public IDictionary<string, string> ReadEntry()
+        {
+            var result = new Dictionary<string, string>();
+
+            if (!EndOfStream)
+            {
+                if (null == Headings)
+                {
+                    Headings = new List<string>();
+                    foreach (var heading in Next())
+                    {
+                        Headings.Add(heading);
+                    }
+
+                    Header = Line;
+                }
+            }
+
+            if (!EndOfStream)
+            {
+                var entry = Next();
+                EntryNumber++;
+                if (Headings.Count !=
+                    entry.Count)
+                {
+                    var message = string.Format(
+                        CultureInfo.InvariantCulture,
+                        Resources.CsvStreamReader_ReadEntry_FormatException,
+                        LineNumber);
+                    throw new FormatException(message);
+                }
+
+                for (var i = 0; i < Headings.Count; i++)
+                {
+                    result.Add(Headings[i], entry[i]);
+                }
+            }
+
+            return result;
+        }
+
+        private IList<string> Next()
+        {
+            var result = new List<string>();
+
+            if (!EndOfStream)
+            {
+                var buffer = new StringBuilder();
+                Line = ReadLine();
+                LineNumber++;
+                var quote = false;
+                foreach (var c in Line)
+                {
+                    switch (c)
+                    {
+                        case ',':
+                            if (quote)
+                            {
+                                buffer.Append(c);
+                            }
+                            else
+                            {
+                                result.Add(buffer.ToString());
+                                buffer.Remove(0, buffer.Length);
+                            }
+
+                            break;
+
+                        case '"':
+                            if (quote)
+                            {
+                                quote = false;
+                            }
+                            else if (0 == buffer.Length)
+                            {
+                                quote = true;
+                            }
+
+                            break;
+
+                        default:
+                            buffer.Append(c);
+                            break;
+                    }
+                }
+
+                result.Add(buffer.ToString());
+            }
+
+            return result;
+        }
+    }
+}
