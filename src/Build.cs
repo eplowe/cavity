2bd16830ef5e9439d73d65fc26df6141806682e3
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2010-09-13 19:44:24 +0000

    Housekeeping.

--- src/Build.cs
+++ src/Build.cs
@@ -14,8 +14,8 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.0.0.79")]
-[assembly: AssemblyFileVersion("1.0.0.79")]
-[assembly: AssemblyInformationalVersion("1.0.0.79")]
+[assembly: AssemblyVersion("1.0.0.80")]
+[assembly: AssemblyFileVersion("1.0.0.80")]
+[assembly: AssemblyInformationalVersion("1.0.0.80")]
 
 
