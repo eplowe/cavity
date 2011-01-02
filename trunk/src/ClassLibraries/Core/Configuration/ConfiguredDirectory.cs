5337552c00f72b9121bcba4e607970adf220c37a
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2011-01-02 02:57:06 +0000

    Various additional string extension methods and implemented support for configured directories.

---
+++ trunk/src/ClassLibraries/Core/Configuration/ConfiguredDirectory.cs
@@ -0,0 +1,80 @@
+﻿namespace Cavity.Configuration
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Configuration;
+    using System.IO;
+    using System.Xml;
+    using Cavity.Properties;
+
+    public sealed class ConfiguredDirectory : IConfigurationSectionHandler
+    {
+        [ThreadStatic]
+        private static IDictionary<string, DirectoryInfo> _mock;
+
+        public static IDictionary<string, DirectoryInfo> Mock
+        {
+            get
+            {
+                return _mock;
+            }
+
+            set
+            {
+                _mock = value;
+            }
+        }
+
+        public static DirectoryInfo Item(string name)
+        {
+            var settings = _mock ?? Settings();
+
+            return settings[name];
+        }
+
+        public static IDictionary<string, DirectoryInfo> Settings()
+        {
+            return Settings("directories");
+        }
+
+        public static IDictionary<string, DirectoryInfo> Settings(string sectionName)
+        {
+            return ConfigurationManager.GetSection(sectionName) as IDictionary<string, DirectoryInfo>;
+        }
+
+        public object Create(object parent, object configContext, XmlNode section)
+        {
+            var result = new Dictionary<string, DirectoryInfo>();
+
+            try
+            {
+                if (null == section)
+                {
+                    throw new XmlException(Resources.ConfiguredList_NullSectionMessage);
+                }
+
+                foreach (XmlNode directory in section.ChildNodes)
+                {
+                    if (null == directory)
+                    {
+                        continue;
+                    }
+
+                    if (null == directory.Attributes)
+                    {
+                        continue;
+                    }
+
+                    var name = directory.Attributes["name"].Value;
+                    result.Add(name, new DirectoryInfo(directory.InnerText));
+                }
+            }
+            catch (Exception exception)
+            {
+                throw new ConfigurationErrorsException(exception.Message, exception, section);
+            }
+
+            return result;
+        }
+    }
+}
