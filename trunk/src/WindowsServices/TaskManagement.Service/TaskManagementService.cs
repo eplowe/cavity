02e934dbdd9acb84a3e6a9eea63891bf4532de6b
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2011-02-12 09:13:03 +0000

    Fixed StyleCop violations.

--- trunk/src/WindowsServices/TaskManagement.Service/TaskManagementService.cs
+++ trunk/src/WindowsServices/TaskManagement.Service/TaskManagementService.cs
@@ -1,15 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Diagnostics;
-using System.Linq;
-using System.ServiceProcess;
-using System.Text;
-
-namespace Cavity
+﻿namespace Cavity
 {
-    partial class TaskManagementService : ServiceBase
+    using System.ServiceProcess;
+
+    internal partial class TaskManagementService : ServiceBase
     {
         public TaskManagementService()
         {
@@ -18,12 +11,10 @@ namespace Cavity
 
         protected override void OnStart(string[] args)
         {
-            // TODO: Add code here to start your service.
         }
 
         protected override void OnStop()
         {
-            // TODO: Add code here to perform any tear-down necessary to stop your service.
         }
     }
 }
