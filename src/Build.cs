a20bb0ee9e1e46cfb77fbc4ee1acdd39590c6866
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2011-02-13 11:26:41 +0000

    Added .user file delete to build file.

--- src/Build.cs
+++ src/Build.cs
@@ -14,8 +14,8 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.0.0.118")]
-[assembly: AssemblyFileVersion("1.0.0.118")]
-[assembly: AssemblyInformationalVersion("1.0.0.118")]
+[assembly: AssemblyVersion("1.0.0.119")]
+[assembly: AssemblyFileVersion("1.0.0.119")]
+[assembly: AssemblyInformationalVersion("1.0.0.119")]
 
 
