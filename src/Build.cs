3458b6f1d02ff4579ecf145d0a85248543611715
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2011-02-20 20:02:21 +0000

    Applied web application conditional compilation.

--- src/Build.cs
+++ src/Build.cs
@@ -14,8 +14,8 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.0.0.126")]
-[assembly: AssemblyFileVersion("1.0.0.126")]
-[assembly: AssemblyInformationalVersion("1.0.0.126")]
+[assembly: AssemblyVersion("1.0.0.127")]
+[assembly: AssemblyFileVersion("1.0.0.127")]
+[assembly: AssemblyInformationalVersion("1.0.0.127")]
 
 
