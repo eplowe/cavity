89df5f16cbe83e52b7dcaefd49454e0b4e585f6f
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2011-02-20 14:35:39 +0000

    Trying to apply conditional at project level.

--- src/Build.cs
+++ src/Build.cs
@@ -14,8 +14,8 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.0.0.123")]
-[assembly: AssemblyFileVersion("1.0.0.123")]
-[assembly: AssemblyInformationalVersion("1.0.0.123")]
+[assembly: AssemblyVersion("1.0.0.125")]
+[assembly: AssemblyFileVersion("1.0.0.125")]
+[assembly: AssemblyInformationalVersion("1.0.0.125")]
 
 
