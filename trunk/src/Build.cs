1e6b309593a5db015a013510a7b0a2e148358574
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2011-02-20 18:50:24 +0000

    More build file rework.

--- trunk/src/Build.cs
+++ trunk/src/Build.cs
@@ -14,8 +14,8 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.0.0.125")]
-[assembly: AssemblyFileVersion("1.0.0.125")]
-[assembly: AssemblyInformationalVersion("1.0.0.125")]
+[assembly: AssemblyVersion("1.0.0.126")]
+[assembly: AssemblyFileVersion("1.0.0.126")]
+[assembly: AssemblyInformationalVersion("1.0.0.126")]
 
 
