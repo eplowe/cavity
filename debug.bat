92bb1d0dda105372034634bb84eb6c3a54a56276
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2011-02-20 21:51:07 +0000

    More build file rework.

--- debug.bat
+++ debug.bat
@@ -1,3 +1,3 @@
-MSBUILD build.xml /p:Versioning=Subversion /p:TargetFrameworkVersion=v3.5 /p:Configuration=Debug
-
 MSBUILD build.xml /p:Versioning=Subversion /p:TargetFrameworkVersion=v4.0 /p:Configuration=Debug
+
+MSBUILD build.xml /p:Versioning=Subversion /p:TargetFrameworkVersion=v3.5 /p:Configuration=Debug
