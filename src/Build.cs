6803a20c7d9bcc82f549a05a1bd0c3d70ca9da51
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2011-02-21 19:31:41 +0000

    Applied conditional build to Unity console app.

--- src/Build.cs
+++ src/Build.cs
@@ -14,8 +14,8 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.0.0.128")]
-[assembly: AssemblyFileVersion("1.0.0.128")]
-[assembly: AssemblyInformationalVersion("1.0.0.128")]
+[assembly: AssemblyVersion("1.0.0.129")]
+[assembly: AssemblyFileVersion("1.0.0.129")]
+[assembly: AssemblyInformationalVersion("1.0.0.129")]
 
 
