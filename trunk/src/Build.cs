ba643fae987a8689bb6a798e27436b079b46ed9e
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2011-02-21 19:48:31 +0000

    Applied conditional build to Castle Windsor console app.

--- trunk/src/Build.cs
+++ trunk/src/Build.cs
@@ -14,8 +14,8 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.0.0.130")]
-[assembly: AssemblyFileVersion("1.0.0.130")]
-[assembly: AssemblyInformationalVersion("1.0.0.130")]
+[assembly: AssemblyVersion("1.0.0.131")]
+[assembly: AssemblyFileVersion("1.0.0.131")]
+[assembly: AssemblyInformationalVersion("1.0.0.131")]
 
 
