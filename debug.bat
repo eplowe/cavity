0c52f84054474811b3b9d0b4268be90d30a209c5
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2011-02-22 20:21:02 +0000

    Added default condition so that Visual Studio will display the files.

--- debug.bat
+++ debug.bat
@@ -1,5 +1,5 @@
 MSBUILD build.xml /p:Versioning=Subversion /p:TargetFrameworkVersion=v4.0 /p:Configuration=Debug
-
+PAUSE
 MSBUILD build.xml /p:Versioning=Subversion /p:TargetFrameworkVersion=v3.5 /p:Configuration=Debug
-
+PAUSE
 MSBUILD build.xml /p:Versioning=Subversion /p:TargetFrameworkVersion=v2.0 /p:Configuration=Debug
