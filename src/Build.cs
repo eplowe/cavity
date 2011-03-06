600edfc33d3e0dabc7f36c0e3b00060ed3b4715c
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2011-03-06 15:01:08 +0000

    Refactoring .csproj layout.

--- src/Build.cs
+++ src/Build.cs
@@ -14,8 +14,8 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.0.0.148")]
-[assembly: AssemblyFileVersion("1.0.0.148")]
-[assembly: AssemblyInformationalVersion("1.0.0.148")]
+[assembly: AssemblyVersion("1.0.0.149")]
+[assembly: AssemblyFileVersion("1.0.0.149")]
+[assembly: AssemblyInformationalVersion("1.0.0.149")]
 
 
