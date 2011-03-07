9e107f6432365fd7c3df9c51f22427d78ef9acb4
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2011-03-07 18:27:44 +0000

    Reintegrated zip building.

--- release.bat
+++ release.bat
@@ -6,7 +6,10 @@ MSBUILD build.xml /p:Versioning=Subversion /p:TargetFrameworkVersion=v3.5 /p:Con
 PAUSE
 MSBUILD build.xml /p:Versioning=Subversion /p:TargetFrameworkVersion=v2.0 /p:Configuration=Release
 
+REM +++++++++++++++++++++++
 REM +++ NEXT : BUNDLING +++
+REM +++++++++++++++++++++++
+
 PAUSE
 MSBUILD bundle.xml /p:Versioning=Subversion /p:TargetFrameworkVersion=v4.0 /p:Configuration=Release
 
