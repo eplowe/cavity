94d9f9a647d321365a83b169e6d968234dfe6df2
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2011-03-07 12:26:27 +0000

    Reintegrating moved / renamed files.

--- trunk/src/Build/Version.cs
+++ trunk/src/Build/Version.cs
@@ -14,8 +14,8 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.0.0.153")]
-[assembly: AssemblyFileVersion("1.0.0.153")]
-[assembly: AssemblyInformationalVersion("1.0.0.153")]
+[assembly: AssemblyVersion("1.0.0.160")]
+[assembly: AssemblyFileVersion("1.0.0.160")]
+[assembly: AssemblyInformationalVersion("1.0.0.160")]
 
 
