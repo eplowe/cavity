fa6831a3479f4db9b6dcd62c78bcbfc574ab678f
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2011-03-07 01:55:59 +0000

    Refactoring .csproj layout.

--- trunk/release.bat
+++ trunk/release.bat
@@ -1,8 +1,17 @@
 MSBUILD build.xml /p:Versioning=Subversion /p:TargetFrameworkVersion=v4.0 /p:Configuration=Release
-MSBUILD bundle.xml /p:Versioning=Subversion /p:TargetFrameworkVersion=v4.0 /p:Configuration=Release
+
 PAUSE
 MSBUILD build.xml /p:Versioning=Subversion /p:TargetFrameworkVersion=v3.5 /p:Configuration=Release
-MSBUILD bundle.xml /p:Versioning=Subversion /p:TargetFrameworkVersion=v3.5 /p:Configuration=Release
+
 PAUSE
 MSBUILD build.xml /p:Versioning=Subversion /p:TargetFrameworkVersion=v2.0 /p:Configuration=Release
+
+REM +++ NEXT : BUNDLING +++
+PAUSE
+MSBUILD bundle.xml /p:Versioning=Subversion /p:TargetFrameworkVersion=v4.0 /p:Configuration=Release
+
+PAUSE
+MSBUILD bundle.xml /p:Versioning=Subversion /p:TargetFrameworkVersion=v3.5 /p:Configuration=Release
+
+PAUSE
 MSBUILD bundle.xml /p:Versioning=Subversion /p:TargetFrameworkVersion=v2.0 /p:Configuration=Release
