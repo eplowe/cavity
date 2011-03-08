c6dc2f7dab797a45a49a7ad81e01b272263da0f8
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2011-03-08 11:42:22 +0000

    Shared service location references; added Record class.

---
+++ src/ClassLibraries/Core.Facts/Data/Record.Facts.cs
@@ -0,0 +1,93 @@
+﻿namespace Cavity.Data
+{
+    using System;
+    using Cavity;
+    using Moq;
+    using Xunit;
+
+    public sealed class RecordFacts
+    {
+        [Fact]
+        public void a_definition()
+        {
+            Assert.True(new TypeExpectations<Record>()
+                .DerivesFrom<ValueObject<Record>>()
+                .IsAbstractBaseClass()
+                .IsNotDecorated()
+                .Implements<IRecord>()
+                .Result);
+        }
+
+        [Fact]
+        public void prop_Cacheability()
+        {
+            Assert.True(new PropertyExpectations<Record>(x => x.Cacheability)
+                            .TypeIs<string>()
+                            .XmlIgnore()
+                            .Result);
+        }
+
+        [Fact]
+        public void prop_Created()
+        {
+            Assert.True(new PropertyExpectations<Record>(x => x.Created)
+                            .TypeIs<DateTime?>()
+                            .XmlIgnore()
+                            .Result);
+        }
+
+        [Fact]
+        public void prop_Etag()
+        {
+            Assert.True(new PropertyExpectations<Record>(x => x.Etag)
+                            .TypeIs<string>()
+                            .XmlIgnore()
+                            .Result);
+        }
+
+        [Fact]
+        public void prop_Expiration()
+        {
+            Assert.True(new PropertyExpectations<Record>(x => x.Expiration)
+                            .TypeIs<string>()
+                            .XmlIgnore()
+                            .Result);
+        }
+
+        [Fact]
+        public void prop_Key()
+        {
+            Assert.True(new PropertyExpectations<Record>(x => x.Key)
+                            .TypeIs<AlphaDecimal?>()
+                            .XmlIgnore()
+                            .Result);
+        }
+
+        [Fact]
+        public void prop_Modified()
+        {
+            Assert.True(new PropertyExpectations<Record>(x => x.Modified)
+                            .TypeIs<DateTime?>()
+                            .XmlIgnore()
+                            .Result);
+        }
+
+        [Fact]
+        public void prop_Status()
+        {
+            Assert.True(new PropertyExpectations<Record>(x => x.Status)
+                            .TypeIs<int?>()
+                            .XmlIgnore()
+                            .Result);
+        }
+
+        [Fact]
+        public void prop_Urn()
+        {
+            Assert.True(new PropertyExpectations<Record>(x => x.Urn)
+                            .TypeIs<AbsoluteUri>()
+                            .XmlIgnore()
+                            .Result);
+        }
+    }
+}
