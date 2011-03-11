1daa5375b67e16168c534d093a422330125a07ec
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2011-03-11 14:24:47 +0000

    Additional repository verifications.

---
+++ src/ClassLibraries/Testing.Unit.Facts/Data/RepositoryUpdateRecordStatusNull`1.Facts.cs
@@ -0,0 +1,97 @@
+﻿namespace Cavity.Data
+{
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+    using Cavity.Tests;
+    using Moq;
+    using Xunit;
+
+    public sealed class RepositoryUpdateRecordStatusNullOfTFacts
+    {
+        [Fact]
+        public void a_definition()
+        {
+            Assert.True(new TypeExpectations<RepositoryUpdateRecordStatusNull<int>>()
+                            .DerivesFrom<object>()
+                            .IsConcreteClass()
+                            .IsSealed()
+                            .HasDefaultConstructor()
+                            .IsNotDecorated()
+                            .Implements<IVerifyRepository<int>>()
+                            .Result);
+        }
+
+        [Fact]
+        public void ctor()
+        {
+            Assert.NotNull(new RepositoryUpdateRecordStatusNull<int>());
+        }
+
+        [Fact]
+        [SuppressMessage("Microsoft.Usage", "CA2208:InstantiateArgumentExceptionsCorrectly", Justification = "This is only for testing purposes.")]
+        public void op_Verify_IRepository()
+        {
+            var obj = new RepositoryUpdateRecordStatusNull<int>();
+
+            var repository = new Mock<IRepository<int>>();
+            repository
+                .Setup(x => x.Insert(obj.Record.Object))
+                .Returns(obj.Record.Object)
+                .Verifiable();
+            repository
+                .Setup(x => x.Update(obj.Record2.Object))
+                .Throws(new RepositoryException())
+                .Verifiable();
+
+            obj.Verify(repository.Object);
+
+            repository.VerifyAll();
+        }
+
+        [Fact]
+        public void op_Verify_IRepositoryNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => new RepositoryUpdateRecordStatusNull<int>().Verify(null));
+        }
+
+        [Fact]
+        public void op_Verify_IRepository_whenExceptionIsNotThrown()
+        {
+            var obj = new RepositoryUpdateRecordStatusNull<int>();
+
+            var repository = new Mock<IRepository<int>>();
+            repository
+                .Setup(x => x.Insert(obj.Record.Object))
+                .Returns(obj.Record.Object)
+                .Verifiable();
+            repository
+                .Setup(x => x.Update(obj.Record2.Object))
+                .Returns(false)
+                .Verifiable();
+
+            Assert.Throws<UnitTestException>(() => obj.Verify(repository.Object));
+
+            repository.VerifyAll();
+        }
+
+        [Fact]
+        public void op_Verify_IRepository_whenExceptionIsUnexpectedlyThrown()
+        {
+            var obj = new RepositoryUpdateRecordStatusNull<int>();
+
+            var repository = new Mock<IRepository<int>>();
+            repository
+                .Setup(x => x.Insert(obj.Record.Object))
+                .Returns(obj.Record.Object)
+                .Verifiable();
+            repository
+                .Setup(x => x.Update(obj.Record2.Object))
+                .Throws(new InvalidOperationException())
+                .Verifiable();
+
+            Assert.Throws<UnitTestException>(() => obj.Verify(repository.Object));
+
+            repository.VerifyAll();
+        }
+    }
+}
---
+++ src/ClassLibraries/Testing.Unit/Data/RepositoryUpdateRecordStatusNull`1.cs
@@ -0,0 +1,88 @@
+﻿namespace Cavity.Data
+{
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Transactions;
+    using Cavity.Properties;
+    using Cavity.Tests;
+    using Moq;
+
+    public sealed class RepositoryUpdateRecordStatusNull<T> : IVerifyRepository<T>
+    {
+        public RepositoryUpdateRecordStatusNull()
+        {
+            AbsoluteUri urn = "urn://example.com/" + Guid.NewGuid();
+
+            var record = new Mock<IRecord<T>>();
+            record
+                .SetupGet(x => x.Cacheability)
+                .Returns("public");
+            record
+                .SetupGet(x => x.Expiration)
+                .Returns("P1D");
+            record
+                .SetupProperty(x => x.Key);
+            record
+                .SetupGet(x => x.Status)
+                .Returns(200);
+            record
+                .SetupGet(x => x.Urn)
+                .Returns(urn);
+            Record = record;
+
+            var record2 = new Mock<IRecord<T>>();
+            record2
+                .SetupGet(x => x.Cacheability)
+                .Returns("public");
+            record2
+                .SetupGet(x => x.Expiration)
+                .Returns("P1D");
+            record2
+                .SetupProperty(x => x.Key);
+            record2
+                .SetupProperty(x => x.Status);
+            record2
+                .SetupGet(x => x.Urn)
+                .Returns(urn);
+            Record2 = record2;
+        }
+
+        [SuppressMessage("Microsoft.Design", "CA1006:DoNotNestGenericTypesInMemberSignatures", Justification = "This is required for mocking.")]
+        public Mock<IRecord<T>> Record { get; set; }
+
+        [SuppressMessage("Microsoft.Design", "CA1006:DoNotNestGenericTypesInMemberSignatures", Justification = "This is required for mocking.")]
+        public Mock<IRecord<T>> Record2 { get; set; }
+
+        public void Verify(IRepository<T> repository)
+        {
+            if (null == repository)
+            {
+                throw new ArgumentNullException("repository");
+            }
+
+            using (new TransactionScope())
+            {
+                Record2.Object.Key = repository.Insert(Record.Object).Key;
+
+                RepositoryException expected = null;
+                try
+                {
+                    repository.Update(Record2.Object);
+                }
+                catch (RepositoryException exception)
+                {
+                    expected = exception;
+                }
+                catch (Exception exception)
+                {
+                    throw new UnitTestException(Resources.Repository_Update_RecordStatusNull_UnitTestExceptionMessage, exception);
+                }
+
+                if (null == expected)
+                {
+                    throw new UnitTestException(Resources.Repository_Update_RecordStatusNull_UnitTestExceptionMessage);
+                }
+            }
+        }
+    }
+}
