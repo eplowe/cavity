34b346a02f4f00b8ab8285511696a235581a626d
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2011-03-13 19:11:43 +0000

    Implemented MD5 hash.

--- trunk/src/Build/Version.cs
+++ trunk/src/Build/Version.cs
@@ -14,8 +14,8 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.0.0.204")]
-[assembly: AssemblyFileVersion("1.0.0.204")]
-[assembly: AssemblyInformationalVersion("1.0.0.204")]
+[assembly: AssemblyVersion("1.0.0.205")]
+[assembly: AssemblyFileVersion("1.0.0.205")]
+[assembly: AssemblyInformationalVersion("1.0.0.205")]
 
 
