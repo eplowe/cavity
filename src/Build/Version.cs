77549b2e29b33010f4e92742e00832f7984e1275
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2011-03-19 09:53:51 +0000

    Created a project include for facts.

--- src/Build/Version.cs
+++ src/Build/Version.cs
@@ -14,8 +14,8 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.0.0.207")]
-[assembly: AssemblyFileVersion("1.0.0.207")]
-[assembly: AssemblyInformationalVersion("1.0.0.207")]
+[assembly: AssemblyVersion("1.0.0.208")]
+[assembly: AssemblyFileVersion("1.0.0.208")]
+[assembly: AssemblyInformationalVersion("1.0.0.208")]
 
 
