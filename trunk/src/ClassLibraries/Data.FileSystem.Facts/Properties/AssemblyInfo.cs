67d3e6a88f001d2511f9e68e7c9ce73f95239df0
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2011-03-19 09:06:35 +0000

    Created new (empty) file system data projects.

---
+++ trunk/src/ClassLibraries/Data.FileSystem.Facts/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection;
+
+[assembly: CLSCompliant(true)]
+[assembly: AssemblyDefaultAlias("Cavity.Data.FileSystem.Facts.dll")]
+[assembly: AssemblyTitle("Cavity.Data.FileSystem.Facts.dll")]
+
+#if (DEBUG)
+
+[assembly: AssemblyDescription("Cavity : File System Data Facts Library (Debug)")]
+
+#else
+
+[assembly: AssemblyDescription("Cavity : File System Data Facts Library (Release)")]
+
+#endif
