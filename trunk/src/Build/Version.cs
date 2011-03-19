360082a24fdff35c9ea1091ec06d4f10c119a9aa
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2011-03-19 11:21:09 +0000

    Refactoring project files.

--- trunk/src/Build/Version.cs
+++ trunk/src/Build/Version.cs
@@ -14,8 +14,8 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.0.0.209")]
-[assembly: AssemblyFileVersion("1.0.0.209")]
-[assembly: AssemblyInformationalVersion("1.0.0.209")]
+[assembly: AssemblyVersion("1.0.0.210")]
+[assembly: AssemblyFileVersion("1.0.0.210")]
+[assembly: AssemblyInformationalVersion("1.0.0.210")]
 
 
