358c00ff42732901c20464723621e95d7c8225ed
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2011-03-19 10:38:51 +0000

    Refactoring project files to derive assembly info.

--- trunk/src/Build/Version.cs
+++ trunk/src/Build/Version.cs
@@ -14,8 +14,8 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.0.0.208")]
-[assembly: AssemblyFileVersion("1.0.0.208")]
-[assembly: AssemblyInformationalVersion("1.0.0.208")]
+[assembly: AssemblyVersion("1.0.0.209")]
+[assembly: AssemblyFileVersion("1.0.0.209")]
+[assembly: AssemblyInformationalVersion("1.0.0.209")]
 
 
