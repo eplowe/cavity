586f126715bb72f1db211cabba5639b33470766b
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2011-03-19 09:06:35 +0000

    Created new (empty) file system data projects.

--- src/Build/Version.cs
+++ src/Build/Version.cs
@@ -14,8 +14,8 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.0.0.206")]
-[assembly: AssemblyFileVersion("1.0.0.206")]
-[assembly: AssemblyInformationalVersion("1.0.0.206")]
+[assembly: AssemblyVersion("1.0.0.207")]
+[assembly: AssemblyFileVersion("1.0.0.207")]
+[assembly: AssemblyInformationalVersion("1.0.0.207")]
 
 
---
+++ src/ClassLibraries/Data.FileSystem/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection;
+
+[assembly: CLSCompliant(true)]
+[assembly: AssemblyDefaultAlias("Cavity.Data.FileSystem.dll")]
+[assembly: AssemblyTitle("Cavity.Data.FileSystem.dll")]
+
+#if (DEBUG)
+
+[assembly: AssemblyDescription("Cavity : File System Data Library (Debug)")]
+
+#else
+
+[assembly: AssemblyDescription("Cavity : File System Data Library (Release)")]
+
+#endif
