625dfe5c48ac5881ae976d893ef5266446cf3d8d
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2011-03-31 19:27:38 +0000

    Tweaking to integrate to new trove references.

--- src/Import/Version.cs
+++ src/Import/Version.cs
@@ -15,7 +15,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.219")]
-[assembly: AssemblyInformationalVersion("1.0.0.219")]
+[assembly: AssemblyFileVersion("1.0.0.220")]
+[assembly: AssemblyInformationalVersion("1.0.0.220")]
 
 
