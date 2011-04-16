87b10d89c87260675325ef09a497eaf5dc240754
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2011-04-16 15:18:42 +0000

    Containing recce code.

--- src/ClassLibraries/Build.Tasks/Build/MessageCompilation.cs
+++ src/ClassLibraries/Build.Tasks/Build/MessageCompilation.cs
@@ -1,12 +1,41 @@
 ﻿namespace Cavity.Build
 {
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using Cavity.Build.Sdk;
+    using Microsoft.Build.Framework;
     using Microsoft.Build.Utilities;
 
     public sealed class MessageCompilation : Task
     {
+        [Required]
+        public ITaskItem[] Files { get; set; }
+
+        [Required]
+        public string WorkingDirectory { get; set; }
+
         public override bool Execute()
         {
-            return false;
+            try
+            {
+                var dir = new DirectoryInfo(WorkingDirectory);
+                var output = MessageCompiler.Current.Compile(dir, ToFiles(Files)).Output;
+                Log.LogMessage(MessageImportance.Normal, output);
+
+                return true;
+            }
+            catch (InvalidOperationException exception)
+            {
+                Log.LogError(exception.Message);
+                return false;
+            }
+        }
+
+        private static IEnumerable<FileInfo> ToFiles(IEnumerable<ITaskItem> files)
+        {
+            return files.Select(file => new FileInfo(file.ItemSpec));
         }
     }
 }
