d44661c549952fdd7731f4fe9393c6e7a87a99e9
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2011-04-17 13:48:21 +0000

    Completed work on build support for message compilation.

--- trunk/src/ClassLibraries/Build.Tasks/Build/MessageLibrary.cs
+++ trunk/src/ClassLibraries/Build.Tasks/Build/MessageLibrary.cs
@@ -5,6 +5,7 @@
     using System.IO;
     using System.Linq;
     using Cavity.Build.Sdk;
+    using Cavity.IO;
     using Microsoft.Build.Framework;
     using Microsoft.Build.Utilities;
 
@@ -14,16 +15,65 @@
         public ITaskItem[] Files { get; set; }
 
         [Required]
+        public string Output { get; set; }
+
+        [Required]
         public string WorkingDirectory { get; set; }
 
         public override bool Execute()
         {
+            if (string.IsNullOrEmpty(WorkingDirectory))
+            {
+                using (var temp = new TempDirectory())
+                {
+                    return Execute(temp.Info);
+                }
+            }
+
+            return Execute(new DirectoryInfo(WorkingDirectory));
+        }
+
+        private static IEnumerable<FileInfo> ToFiles(IEnumerable<ITaskItem> files)
+        {
+            return files.Select(file => new FileInfo(file.ItemSpec));
+        }
+
+        private void CompileMessages(DirectoryInfo workingDirectory)
+        {
+            if (!workingDirectory.Exists)
+            {
+                workingDirectory.Create();
+            }
+
+            foreach (var file in ToFiles(Files))
+            {
+                var source = file.FullName;
+                var destination = new FileInfo(Path.Combine(workingDirectory.FullName, file.Name)).FullName;
+
+                File.Copy(source, destination);
+            }
+
+            Log.LogMessage(
+                MessageImportance.Normal,
+                MessageCompiler.Current.Compile(workingDirectory, workingDirectory.GetFiles("*.mc", SearchOption.TopDirectoryOnly)));
+        }
+
+        private void CompileResources(DirectoryInfo workingDirectory)
+        {
+            Log.LogMessage(
+                MessageImportance.Normal,
+                ResourceCompiler.Current.Compile(workingDirectory, workingDirectory.GetFiles("*.rc", SearchOption.TopDirectoryOnly)));
+        }
+
+        private bool Execute(DirectoryInfo workingDirectory)
+        {
             try
             {
-                var dir = new DirectoryInfo(WorkingDirectory);
-                var output = MessageCompiler.Current.Compile(dir, ToFiles(Files));
-                Log.LogMessage(MessageImportance.Normal, output);
+                CompileMessages(workingDirectory);
+                CompileResources(workingDirectory);
+                LinkResources(workingDirectory);
 
+                Log.LogMessage(MessageImportance.Normal, "Compiled {0}".FormatWith(Output));
                 return true;
             }
             catch (InvalidOperationException exception)
@@ -33,9 +83,25 @@
             }
         }
 
-        private static IEnumerable<FileInfo> ToFiles(IEnumerable<ITaskItem> files)
+        private void LinkResources(DirectoryInfo workingDirectory)
         {
-            return files.Select(file => new FileInfo(file.ItemSpec));
+            LinkResources(workingDirectory, new FileInfo(Output));
+        }
+
+        private void LinkResources(DirectoryInfo workingDirectory,
+                                   FileInfo output)
+        {
+            if (!output.Directory.Exists)
+            {
+                output.Directory.Create();
+            }
+
+            var link = LinkCompiler.Current;
+            link.Out = output;
+
+            Log.LogMessage(
+                MessageImportance.Normal,
+                link.Compile(workingDirectory, workingDirectory.GetFiles("*.res", SearchOption.TopDirectoryOnly)));
         }
     }
 }
---
+++ trunk/src/ClassLibraries/Build.Tasks/Build/Sdk/LinkCompiler.cs
@@ -0,0 +1,36 @@
+﻿namespace Cavity.Build.Sdk
+{
+    using System.Collections.Generic;
+    using System.IO;
+
+    /// <summary>
+    /// Encapsulates access to the Platform SDK linking compiler.
+    /// </summary>
+    /// <seealso href="http://msdn.microsoft.com/library/t2fck18t">
+    /// Linking
+    /// </seealso>
+    public sealed class LinkCompiler : CompilerBase
+    {
+        private LinkCompiler(string location)
+            : base(location)
+        {
+        }
+
+        public static LinkCompiler Current
+        {
+            get
+            {
+                return new LinkCompiler("LINK.exe");
+            }
+        }
+
+        public string Machine { get; set; }
+
+        public FileInfo Out { get; set; }
+
+        public override string ToArguments(IEnumerable<string> files)
+        {
+            return ToArguments("-dll -noentry -machine:{1} -out:\"{0}\"".FormatWith(Out.FullName, Machine ?? "X86"), files);
+        }
+    }
+}
