bfc85dc99b318a11ebdc32226645933b4780fd3d
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2011-04-22 17:41:20 +0000

    Added setup project for message libs.

--- trunk/src/Import/Version.cs
+++ trunk/src/Import/Version.cs
@@ -15,7 +15,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.244")]
-[assembly: AssemblyInformationalVersion("1.0.0.244")]
+[assembly: AssemblyFileVersion("1.0.0.248")]
+[assembly: AssemblyInformationalVersion("1.0.0.248")]
 
 
