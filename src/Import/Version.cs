b407b716743d0bea6d4a00e5ad6cd06c5f0e6fdb
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2011-04-22 18:27:17 +0000

    Tweaked conditionality to build on default (i.e. in Visual Studio).

--- src/Import/Version.cs
+++ src/Import/Version.cs
@@ -15,7 +15,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.248")]
-[assembly: AssemblyInformationalVersion("1.0.0.248")]
+[assembly: AssemblyFileVersion("1.0.0.249")]
+[assembly: AssemblyInformationalVersion("1.0.0.249")]
 
 
