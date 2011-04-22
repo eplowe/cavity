b111c66e6a0fc56402458872fd954c34df9d3330
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2011-04-22 07:38:20 +0000

    Changed tack: deleted custom task and implemented C++ project instead.

--- src/ClassLibraries/Build.Tasks.Facts/Diagnostics/FakeProcess.cs
+++ src/ClassLibraries/Build.Tasks.Facts/Diagnostics/FakeProcess.cs
@@ -1,62 +1,62 @@
-﻿namespace Cavity.Diagnostics
-{
-    using System.Diagnostics;
-    using System.IO;
+﻿////namespace Cavity.Diagnostics
+////{
+////    using System.Diagnostics;
+////    using System.IO;
 
-    public sealed class FakeProcess : IProcess
-    {
-        public int ExitCode
-        {
-            get
-            {
-                return 0;
-            }
-        }
+////    public sealed class FakeProcess : IProcess
+////    {
+////        public int ExitCode
+////        {
+////            get
+////            {
+////                return 0;
+////            }
+////        }
 
-        public StreamReader StandardError
-        {
-            get
-            {
-                var stream = new MemoryStream();
-                var writer = new StreamWriter(stream);
-                var reader = new StreamReader(stream);
-                writer.Write("error");
-                writer.Flush();
-                stream.Position = 0;
+////        public StreamReader StandardError
+////        {
+////            get
+////            {
+////                var stream = new MemoryStream();
+////                var writer = new StreamWriter(stream);
+////                var reader = new StreamReader(stream);
+////                writer.Write("error");
+////                writer.Flush();
+////                stream.Position = 0;
 
-                return reader;
-            }
-        }
+////                return reader;
+////            }
+////        }
 
-        public StreamReader StandardOutput
-        {
-            get
-            {
-                var stream = new MemoryStream();
-                var writer = new StreamWriter(stream);
-                var reader = new StreamReader(stream);
-                writer.Write("fake");
-                writer.Flush();
-                stream.Position = 0;
+////        public StreamReader StandardOutput
+////        {
+////            get
+////            {
+////                var stream = new MemoryStream();
+////                var writer = new StreamWriter(stream);
+////                var reader = new StreamReader(stream);
+////                writer.Write("fake");
+////                writer.Flush();
+////                stream.Position = 0;
 
-                return reader;
-            }
-        }
+////                return reader;
+////            }
+////        }
 
-        public ProcessStartInfo StartInfo { get; set; }
+////        public ProcessStartInfo StartInfo { get; set; }
 
-        public void Dispose()
-        {
-        }
+////        public void Dispose()
+////        {
+////        }
 
-        public bool Start()
-        {
-            return true;
-        }
+////        public bool Start()
+////        {
+////            return true;
+////        }
 
-        public bool WaitForExit(int milliseconds)
-        {
-            return true;
-        }
-    }
-}
+////        public bool WaitForExit(int milliseconds)
+////        {
+////            return true;
+////        }
+////    }
+////}
--- src/Import/Version.cs
+++ src/Import/Version.cs
@@ -15,7 +15,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.243")]
-[assembly: AssemblyInformationalVersion("1.0.0.243")]
+[assembly: AssemblyFileVersion("1.0.0.244")]
+[assembly: AssemblyInformationalVersion("1.0.0.244")]
 
 
