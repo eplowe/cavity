3942a73d11f007d4997d024ce617ce3f9311d667
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2011-04-23 20:01:36 +0000

    Integrating diagnostics into the task management service.

---
+++ src/ClassLibraries/TaskManagement.Facts/Models/TaskManager.Facts.cs
@@ -0,0 +1,68 @@
+﻿namespace Cavity.Models
+{
+    using Cavity;
+    using Xunit;
+
+    public sealed class TaskManagerFacts
+    {
+        [Fact]
+        public void a_definition()
+        {
+            Assert.True(new TypeExpectations<TaskManager>()
+                .DerivesFrom<object>()
+                .IsConcreteClass()
+                .IsSealed()
+                .HasDefaultConstructor()
+                .IsNotDecorated()
+                .Result);
+        }
+
+        [Fact]
+        public void ctor()
+        {
+            Assert.NotNull(new TaskManager());
+        }
+
+        [Fact]
+        public void op_Continue()
+        {
+            IManageTasks obj = new TaskManager();
+            obj.Continue();
+        }
+
+        [Fact]
+        public void op_Pause()
+        {
+            IManageTasks obj = new TaskManager();
+            obj.Pause();
+        }
+
+        [Fact]
+        public void op_Shutdown()
+        {
+            IManageTasks obj = new TaskManager();
+            obj.Shutdown();
+        }
+
+        [Fact]
+        public void op_Start_IEnumerableOfString()
+        {
+            IManageTasks obj = new TaskManager();
+            obj.Start(new[] { "abc" });
+        }
+
+        [Fact]
+        public void op_Start_IEnumerableOfStringNull()
+        {
+            IManageTasks obj = new TaskManager();
+            obj.Start(null);
+        }
+
+        [Fact]
+        public void op_Stop()
+        {
+            IManageTasks obj = new TaskManager();
+            obj.Stop();
+        }
+    }
+}
--- src/Import/Version.cs
+++ src/Import/Version.cs
@@ -15,7 +15,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.252")]
-[assembly: AssemblyInformationalVersion("1.0.0.252")]
+[assembly: AssemblyFileVersion("1.0.0.253")]
+[assembly: AssemblyInformationalVersion("1.0.0.253")]
 
 
