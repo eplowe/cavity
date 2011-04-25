db17d0678d2ebe92ff832d37254e16f0c013c5e0
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2011-04-25 11:38:22 +0000

    Implemented config utility method.

--- trunk/src/Import/Version.cs
+++ trunk/src/Import/Version.cs
@@ -15,7 +15,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.257")]
-[assembly: AssemblyInformationalVersion("1.0.0.257")]
+[assembly: AssemblyFileVersion("1.0.0.258")]
+[assembly: AssemblyInformationalVersion("1.0.0.258")]
 
 
