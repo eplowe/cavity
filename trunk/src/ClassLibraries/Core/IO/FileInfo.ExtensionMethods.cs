aa571d575091f54085bab79a10fea928cfdeded6
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2011-05-01 19:24:51 +0000

    Added a couple of extension methods to FileInfo.

---
+++ trunk/src/ClassLibraries/Core/IO/FileInfo.ExtensionMethods.cs
@@ -0,0 +1,45 @@
+﻿namespace Cavity.IO
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+
+    public static class FileInfoExtensionMethods
+    {
+        public static IEnumerable<string> Lines(this FileInfo obj)
+        {
+            if (null == obj)
+            {
+                throw new ArgumentNullException("obj");
+            }
+
+            using (var stream = obj.Open(FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                using (var reader = new StreamReader(stream))
+                {
+                    while (reader.Peek() >
+                           -1)
+                    {
+                        yield return reader.ReadLine();
+                    }
+                }
+            }
+        }
+
+        public static string ReadToEnd(this FileInfo obj)
+        {
+            if (null == obj)
+            {
+                throw new ArgumentNullException("obj");
+            }
+
+            using (var stream = obj.Open(FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                using (var reader = new StreamReader(stream))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+        }
+    }
+}
