445f8a7d2ea17e70ddebe6893dde0fe324205353
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2011-05-03 04:56:00 +0000

    Fixing config issues.

---
+++ src/ClassLibraries/Core.Facts/Configuration/DirectoryConfigurationElement.Facts.cs
@@ -0,0 +1,48 @@
+﻿namespace Cavity.Configuration
+{
+    using System;
+    using System.Configuration;
+    using System.IO;
+    using Cavity;
+    using Cavity.IO;
+    using Xunit;
+
+    public sealed class DirectoryConfigurationElementFacts
+    {
+        [Fact]
+        public void a_definition()
+        {
+            Assert.True(new TypeExpectations<DirectoryConfigurationElement>()
+                            .DerivesFrom<ConfigurationElement>()
+                            .IsConcreteClass()
+                            .IsSealed()
+                            .HasDefaultConstructor()
+                            .IsNotDecorated()
+                            .Result);
+        }
+
+        [Fact]
+        public void ctor()
+        {
+            Assert.NotNull(new DirectoryConfigurationElement());
+        }
+
+        [Fact]
+        public void ctor_DirectoryInfo()
+        {
+            using (var temp = new TempDirectory())
+            {
+                Assert.NotNull(new DirectoryConfigurationElement(temp.Info));
+            }
+        }
+
+        [Fact]
+        public void prop_Directory()
+        {
+            Assert.True(new PropertyExpectations<DirectoryConfigurationElement>(p => p.Directory)
+                            .TypeIs<DirectoryInfo>()
+                            .IsNotDecorated()
+                            .Result);
+        }
+    }
+}
