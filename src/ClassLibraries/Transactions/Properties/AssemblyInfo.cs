e2843c3ec5ea41e798a06e5e838027cce095872f
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2011-05-14 19:05:50 +0000

    Pulling Transactions code into it's own library.

---
+++ src/ClassLibraries/Transactions/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection;
+
+[assembly: CLSCompliant(true)]
+[assembly: AssemblyDefaultAlias("Cavity.Transactions.dll")]
+[assembly: AssemblyTitle("Cavity.Transactions.dll")]
+
+#if (DEBUG)
+
+[assembly: AssemblyDescription("Cavity : Transactions Library (Debug)")]
+
+#else
+
+[assembly: AssemblyDescription("Cavity : Transactions Library (Release)")]
+
+#endif
