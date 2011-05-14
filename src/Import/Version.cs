67e70d2bac8436e724367da037133139b1e0b7a3
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2011-05-14 11:03:55 +0000

    Pulling Repository code into it's own library.

--- src/Import/Version.cs
+++ src/Import/Version.cs
@@ -15,7 +15,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.320")]
-[assembly: AssemblyInformationalVersion("1.0.0.320")]
+[assembly: AssemblyFileVersion("1.0.0.321")]
+[assembly: AssemblyInformationalVersion("1.0.0.321")]
 
 
