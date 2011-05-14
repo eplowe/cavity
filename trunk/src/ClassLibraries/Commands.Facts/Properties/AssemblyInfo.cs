7dfe416c2b1f41d67e6e6ccce4d2f27ae1356f2c
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2011-05-14 06:21:04 +0000

    Created Commands projects.

---
+++ trunk/src/ClassLibraries/Commands.Facts/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection;
+
+[assembly: CLSCompliant(true)]
+[assembly: AssemblyDefaultAlias("Cavity.Commands.Facts.dll")]
+[assembly: AssemblyTitle("Cavity.Commands.Facts.dll")]
+
+#if (DEBUG)
+
+[assembly: AssemblyDescription("Cavity : Commands Facts Library (Debug)")]
+
+#else
+
+[assembly: AssemblyDescription("Cavity : Commands Facts Library (Release)")]
+
+#endif
--- trunk/src/Import/Version.cs
+++ trunk/src/Import/Version.cs
@@ -15,7 +15,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.292")]
-[assembly: AssemblyInformationalVersion("1.0.0.292")]
+[assembly: AssemblyFileVersion("1.0.0.295")]
+[assembly: AssemblyInformationalVersion("1.0.0.295")]
 
 
