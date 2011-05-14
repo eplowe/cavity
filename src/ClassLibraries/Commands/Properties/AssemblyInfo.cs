e0ae7dd0469afde42f91d0ed9ca4b126c2de49d5
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2011-05-14 06:21:04 +0000

    Created Commands projects.

---
+++ src/ClassLibraries/Commands/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection;
+
+[assembly: CLSCompliant(true)]
+[assembly: AssemblyDefaultAlias("Cavity.Commands.dll")]
+[assembly: AssemblyTitle("Cavity.Commands.dll")]
+
+#if (DEBUG)
+
+[assembly: AssemblyDescription("Cavity : Commands Library (Debug)")]
+
+#else
+
+[assembly: AssemblyDescription("Cavity : Commands Library (Release)")]
+
+#endif
