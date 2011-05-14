24fb2d1b17139f33e0a0d4b0cb79a77dd3f9e55f
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2011-05-14 11:03:55 +0000

    Pulling Repository code into it's own library.

---
+++ trunk/src/ClassLibraries/Repository/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection;
+
+[assembly: CLSCompliant(true)]
+[assembly: AssemblyDefaultAlias("Cavity.Repository.dll")]
+[assembly: AssemblyTitle("Cavity.Repository.dll")]
+
+#if (DEBUG)
+
+[assembly: AssemblyDescription("Cavity : Repository Library (Debug)")]
+
+#else
+
+[assembly: AssemblyDescription("Cavity : Repository Library (Release)")]
+
+#endif
