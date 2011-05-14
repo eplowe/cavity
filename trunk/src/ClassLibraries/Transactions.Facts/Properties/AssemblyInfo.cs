914e93c330b0a0d0de593befab4898510e1130bf
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2011-05-14 19:05:50 +0000

    Pulling Transactions code into it's own library.

---
+++ trunk/src/ClassLibraries/Transactions.Facts/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection;
+
+[assembly: CLSCompliant(true)]
+[assembly: AssemblyDefaultAlias("Cavity.Transactions.Facts.dll")]
+[assembly: AssemblyTitle("Cavity.Transactions.Facts.dll")]
+
+#if (DEBUG)
+
+[assembly: AssemblyDescription("Cavity : Transactions Facts Library (Debug)")]
+
+#else
+
+[assembly: AssemblyDescription("Cavity : Transactions Facts Library (Release)")]
+
+#endif
--- trunk/src/Import/Version.cs
+++ trunk/src/Import/Version.cs
@@ -15,7 +15,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.326")]
-[assembly: AssemblyInformationalVersion("1.0.0.326")]
+[assembly: AssemblyFileVersion("1.0.0.328")]
+[assembly: AssemblyInformationalVersion("1.0.0.328")]
 
 
