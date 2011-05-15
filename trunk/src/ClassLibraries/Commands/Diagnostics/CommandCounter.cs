c5cdf955def6b9cd7a32fcfe967222021d804844
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2011-05-15 15:52:04 +0000

    Added commands counter.

---
+++ trunk/src/ClassLibraries/Commands/Diagnostics/CommandCounter.cs
@@ -0,0 +1,29 @@
+﻿namespace Cavity.Diagnostics
+{
+    using System.Diagnostics;
+
+    public static class CommandCounter
+    {
+        private const string Category = "Cavity";
+
+        private const string Counter = "Commands/sec";
+
+        private static readonly bool _exists = PerformanceCounterCategory.CounterExists(Counter, Category);
+
+        public static void Increment()
+        {
+            if (!_exists)
+            {
+                return;
+            }
+
+            using (var counter = new PerformanceCounter(Category, Counter, false)
+            {
+                MachineName = "."
+            })
+            {
+                counter.Increment();
+            }
+        }
+    }
+}
