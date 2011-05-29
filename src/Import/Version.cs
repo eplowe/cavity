39ca44ac65ab22f5c12ea4526aacc5c103a7d2e5
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2011-05-29 08:15:55 +0000

    Additional tracing in Config.Xml<T>()

--- src/Import/Version.cs
+++ src/Import/Version.cs
@@ -15,7 +15,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.336")]
-[assembly: AssemblyInformationalVersion("1.1.0.336")]
+[assembly: AssemblyFileVersion("1.1.0.337")]
+[assembly: AssemblyInformationalVersion("1.1.0.337")]
 
 
