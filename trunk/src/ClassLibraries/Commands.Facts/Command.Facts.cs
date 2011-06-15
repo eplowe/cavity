5388513f0cd47bbf117f13c217706e4d0e2d2839
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2011-06-15 18:02:20 +0000

    Better test coverage.

--- trunk/src/ClassLibraries/Commands.Facts/Command.Facts.cs
+++ trunk/src/ClassLibraries/Commands.Facts/Command.Facts.cs
@@ -1,5 +1,8 @@
 ﻿namespace Cavity
 {
+    using System;
+    using System.Xml.Serialization;
+    using Cavity.Xml.XPath;
     using Xunit;
 
     public sealed class CommandFacts
@@ -13,6 +16,18 @@
                             .IsNotDecorated()
                             .Implements<ICommand>()
                             .Result);
+        }
+
+        [Fact]
+        public void ctor()
+        {
+            Assert.NotNull(new DerivedCommand());
+        }
+
+        [Fact]
+        public void ctor_bool()
+        {
+            Assert.True(new DerivedCommand(true).Unidirectional);
         }
 
         [Fact]
@@ -31,6 +46,45 @@
                             .TypeIs<bool>()
                             .IsNotDecorated()
                             .Result);
+        }
+
+        [Fact]
+        public void op_GetSchema()
+        {
+            Assert.Throws<NotSupportedException>(() => (new DerivedCommand() as IXmlSerializable).GetSchema());
+        }
+
+        [Fact]
+        public void op_ReadXml_XmlReaderNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => (new DerivedCommand() as IXmlSerializable).ReadXml(null));
+        }
+
+        [Fact]
+        public void op_WriteXml_XmlWriterNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => (new DerivedCommand() as IXmlSerializable).WriteXml(null));
+        }
+
+        [Fact]
+        public void serialize()
+        {
+            var obj = new DerivedCommand(true);
+
+            var navigator = obj.XmlSerialize().CreateNavigator();
+
+            Assert.True(navigator.Evaluate<bool>("1 = count(/command)"));
+            const string xpath = "1 = count(command[@undo='false'][@unidirectional='true'])";
+            Assert.True(navigator.Evaluate<bool>(xpath));
+        }
+
+        [Fact]
+        public void deserialize()
+        {
+            var obj = "<command undo='true' unidirectional='true' />".XmlDeserialize<DerivedCommand>();
+
+            Assert.True(obj.Undo);
+            Assert.True(obj.Unidirectional);
         }
     }
 }
