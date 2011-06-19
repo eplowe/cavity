e773e4538ae3374dbc70def4669eeb9b1e45ad74
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2011-06-19 08:58:53 +0000

    Implemented a dynamic data dictionary object.

---
+++ src/ClassLibraries/Core.Facts/Dynamic/DynamicData.Facts.cs
@@ -0,0 +1,75 @@
+﻿namespace Cavity.Dynamic
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Dynamic;
+    using Cavity;
+    using Xunit;
+
+    public sealed class DynamicDataFacts
+    {
+        [Fact]
+        public void a_definition()
+        {
+            Assert.True(new TypeExpectations<DynamicData>()
+                .DerivesFrom<DynamicObject>()
+                .IsConcreteClass()
+                .IsSealed()
+                .HasDefaultConstructor()
+                .IsNotDecorated()
+                .Result);
+        }
+
+        [Fact]
+        public void ctor()
+        {
+            Assert.NotNull(new DynamicData());
+        }
+
+        [Fact]
+        public void example()
+        {
+            const string expected = "example";
+
+            dynamic obj = new DynamicData();
+            obj.Example = expected;
+
+            var actual = obj.Example;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void example_whenNotSet()
+        {
+            dynamic obj = new DynamicData();
+
+            Assert.Throws<KeyNotFoundException>(() => obj.Example);
+        }
+
+        [Fact]
+        public void op_GetDynamicMemberNames()
+        {
+            dynamic obj = new DynamicData();
+            obj.Example = string.Empty;
+
+            foreach (var actual in ((DynamicData)obj).GetDynamicMemberNames())
+            {
+                Assert.Equal("Example", actual);
+            }
+        }
+
+        [Fact]
+        public void op_TryGetMember_GetMemberBinder_object()
+        {
+            object result;
+            Assert.Throws<ArgumentNullException>(() => new DynamicData().TryGetMember(null, out result));
+        }
+
+        [Fact]
+        public void op_TrySetMember_SetMemberBinder_object()
+        {
+            Assert.Throws<ArgumentNullException>(() => new DynamicData().TrySetMember(null, "example"));
+        }
+    }
+}
---
+++ src/ClassLibraries/Core/Dynamic/DynamicData.cs
@@ -0,0 +1,43 @@
+﻿namespace Cavity.Dynamic
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Dynamic;
+
+    public sealed class DynamicData : DynamicObject
+    {
+        private readonly Dictionary<string, object> _data;
+
+        public DynamicData()
+        {
+            _data = new Dictionary<string, object>();
+        }
+
+        public override IEnumerable<string> GetDynamicMemberNames()
+        {
+            return _data.Keys;
+        }
+
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            if (null == binder)
+            {
+                throw new ArgumentNullException("binder");
+            }
+
+            result = _data[binder.Name];
+            return true;
+        }
+
+        public override bool TrySetMember(SetMemberBinder binder, object value)
+        {
+            if (null == binder)
+            {
+                throw new ArgumentNullException("binder");
+            }
+
+            _data[binder.Name] = value;
+            return true;
+        }
+    }
+}
