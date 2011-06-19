c1fa733987a394a4979afed5d243844918333004
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2011-06-19 08:58:53 +0000

    Implemented a dynamic data dictionary object.

--- trunk/src/Import/Version.cs
+++ trunk/src/Import/Version.cs
@@ -15,7 +15,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.353")]
-[assembly: AssemblyInformationalVersion("1.1.0.353")]
+[assembly: AssemblyFileVersion("1.1.0.354")]
+[assembly: AssemblyInformationalVersion("1.1.0.354")]
 
 
