d563d147be6a6bb35426dfb57364f84f75f8f620
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2011-10-09 22:24:36 +0000

    Completed re-org of Cavity.Diagnostics.Log4Net

--- trunk/src/Cavity Diagnostics log4net/Version.cs
+++ trunk/src/Cavity Diagnostics log4net/Version.cs
@@ -15,7 +15,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.367")]
-[assembly: AssemblyInformationalVersion("1.1.0.367")]
+[assembly: AssemblyFileVersion("1.1.0.400")]
+[assembly: AssemblyInformationalVersion("1.1.0.400")]
 
 
---
+++ trunk/src/Cavity Diagnostics log4net/nuget/content/README.txt
@@ -0,0 +1,5 @@
+﻿Please add:
+
+	log4net.Config.XmlConfigurator.Configure();
+	
+to either your Main() or in your Global.asax
