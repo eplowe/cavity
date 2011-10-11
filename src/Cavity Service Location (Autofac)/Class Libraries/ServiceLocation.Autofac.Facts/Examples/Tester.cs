903553e4ade11124da7371079b6083ee3f2dd42d
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2011-10-11 22:10:31 +0000

    Completed re-org of Cavity Service Location (Autofac).

---
+++ src/Cavity Service Location (Autofac)/Class Libraries/ServiceLocation.Autofac.Facts/Examples/Tester.cs
@@ -0,0 +1,19 @@
+﻿namespace Cavity.Examples
+{
+    using System;
+
+    public sealed class Tester : ITest
+    {
+        public Tester(string expected)
+        {
+            Expected = expected;
+        }
+
+        public string Expected { get; set; }
+
+        public bool Test(string actual)
+        {
+            return string.Equals(Expected, actual, StringComparison.Ordinal);
+        }
+    }
+}
