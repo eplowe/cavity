a4079545cbf6b17e65ebbf5a436b6299fcdea2b8
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2011-10-22 07:50:51 +0000

    Moving Domain projects.

---
+++ trunk/src/Cavity MSBuild (Domain)/Class Libraries/MSBuild.Domain.Facts/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection;
+
+[assembly: CLSCompliant(true)]
+[assembly: AssemblyDefaultAlias("Cavity.MSBuild.Domain.Facts.dll")]
+[assembly: AssemblyTitle("Cavity.MSBuild.Domain.Facts.dll")]
+
+#if (DEBUG)
+
+[assembly: AssemblyDescription("Cavity : Domain MSBuild Facts Library (Debug)")]
+
+#else
+
+[assembly: AssemblyDescription("Cavity : Domain MSBuild Facts Library (Release)")]
+
+#endif
