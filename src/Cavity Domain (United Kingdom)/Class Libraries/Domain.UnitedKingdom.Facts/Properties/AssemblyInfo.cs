92cc6a9f42ce4922b131c17daba2fb5cea99787c
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2011-10-22 07:50:51 +0000

    Moving Domain projects.

---
+++ src/Cavity Domain (United Kingdom)/Class Libraries/Domain.UnitedKingdom.Facts/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection;
+
+[assembly: CLSCompliant(true)]
+[assembly: AssemblyDefaultAlias("Cavity.Domain.UnitedKingdom.Facts.dll")]
+[assembly: AssemblyTitle("Cavity.Domain.UnitedKingdom.Facts.dll")]
+
+#if (DEBUG)
+
+[assembly: AssemblyDescription("Cavity : United Kingdom Domain Facts Library (Debug)")]
+
+#else
+
+[assembly: AssemblyDescription("Cavity : United Kingdom Domain Facts Library (Release)")]
+
+#endif
---
+++ src/Cavity Domain (United Kingdom)/Class Libraries/Domain.UnitedKingdom/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+
+[assembly: CLSCompliant(true)]
+[assembly: AssemblyDefaultAlias("Cavity.UnitedKingdom.dll")]
+[assembly: AssemblyTitle("Cavity.UnitedKingdom.dll")]
+
+#if (DEBUG)
+
+[assembly: AssemblyDescription("Cavity : United Kingdom Domain Library (Debug)")]
+
+#else
+
+[assembly: AssemblyDescription("Cavity : United Kingdom Domain Library (Release)")]
+
+#endif
+
+[assembly: SuppressMessage("Microsoft.Design", "CA1020:AvoidNamespacesWithFewTypes", Scope = "namespace", Target = "Cavity")]
