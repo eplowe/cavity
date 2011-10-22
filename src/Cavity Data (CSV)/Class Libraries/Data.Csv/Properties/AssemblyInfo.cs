b89b03d57030487d025d44430db0d2a8ce29d45f
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2011-10-22 09:49:54 +0000

    Moving Data projects.

---
+++ src/Cavity Data (CSV)/Class Libraries/Data.Csv/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+
+[assembly: CLSCompliant(true)]
+[assembly: AssemblyDefaultAlias("Cavity.Data.Csv.dll")]
+[assembly: AssemblyTitle("Cavity.Data.Csv.dll")]
+
+#if (DEBUG)
+
+[assembly: AssemblyDescription("Cavity : CSV Data Library (Debug)")]
+
+#else
+
+[assembly: AssemblyDescription("Cavity : CSV Data Library (Release)")]
+
+#endif
+
+[assembly: SuppressMessage("Microsoft.Design", "CA1020:AvoidNamespacesWithFewTypes", Scope = "namespace", Target = "Cavity.Data")]
