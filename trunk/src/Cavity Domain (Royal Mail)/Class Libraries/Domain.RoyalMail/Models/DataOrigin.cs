9ed3f997d59b6d695863f76ce4d57008dd3d0e23
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2011-10-23 15:12:36 +0000

    Updating to latest packages.

--- trunk/src/Cavity Domain (Royal Mail)/Class Libraries/Domain.RoyalMail/Models/DataOrigin.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Cavity.Models
-{
-    public abstract class DataOrigin
-    {
-        public virtual char Code { get; set; }
-
-        public virtual string Name { get; set; }
-    }
-}
--- trunk/src/Cavity Domain (Royal Mail)/Class Libraries/Domain.RoyalMail/Models/PostalAddressFileEntry.cs
+++ trunk/src/Cavity Domain (Royal Mail)/Class Libraries/Domain.RoyalMail/Models/PostalAddressFileEntry.cs
@@ -20,8 +20,6 @@
 
         public virtual IUserCategory Category { get; set; }
 
-        public virtual DataOrigin Origin { get; set; }
-
         public virtual string DeliveryPointSuffix { get; set; }
 
         public virtual int? MultipleOccupancyCount { get; set; }
@@ -31,6 +29,8 @@
         public virtual int? NumberOfDeliveryPoints { get; set; }
 
         public virtual Organization Organization { get; private set; }
+
+        public virtual char? Origin { get; set; }
 
         public virtual int? SortCode { get; set; }
 
@@ -54,24 +54,31 @@
             {
                 Address =
                 {
-                    SubBuildingName = data["SBN"],
-                    BuildingName = data["BNA"],
-                    BuildingNumber = data["NUM"],
-                    DependentStreet = data["DST"],
-                    MainStreet = data["STM"],
-                    DoubleDependentLocality = data["DDL"],
-                    DependentLocality = data["DLO"],
-                    PostTown = data["PTN"],
-                    PostOfficeBox = data["POB"],
-                    Postcode = data["PCD"]
+                    SubBuildingName = data.ContainsKey("SBN") ? data["SBN"] : null,
+                    BuildingName = data.ContainsKey("BNA") ? data["BNA"] : null,
+                    BuildingNumber = data.ContainsKey("NUM") ? data["NUM"] : null,
+                    DependentStreet = data.ContainsKey("DST") ? data["DST"] : null,
+                    MainStreet = data.ContainsKey("STM") ? data["STM"] : null,
+                    DoubleDependentLocality = data.ContainsKey("DDL") ? data["DDL"] : null,
+                    DependentLocality = data.ContainsKey("DLO") ? data["DLO"] : null,
+                    PostTown = data.ContainsKey("PTN") ? data["PTN"] : null,
+                    PostOfficeBox = data.ContainsKey("POB") ? data["POB"] : null,
+                    Postcode = data.ContainsKey("PCD") ? data["PCD"] : null
                 },
                 Organization = 
                 {
-                    Department = data["ORD"],
-                    Name = data["ORC"]
+                    Department = data.ContainsKey("ORD") ? data["ORD"] : null,
+                    Name = data.ContainsKey("ORC") ? data["ORC"] : null
                 },
-                Category = UserCategory.Resolve(data.Value<char>("CAT")),
-                SortCode = data.Value<int>("SCD")
+                Category = data.ContainsKey("CAT") ? UserCategory.Resolve(data.Value<char>("CAT")) : null,
+                DeliveryPointSuffix = data.ContainsKey("DPX") ? data["DPX"] : null,
+                MultipleOccupancyCount = data.ContainsKey("MOC") ? data.TryValue<int>("MOC") : new int?(),
+                MultipleResidencyRecordCount = data.ContainsKey("MRC") ? data.TryValue<int>("MRC") : new int?(),
+                NumberOfDeliveryPoints = data.ContainsKey("NDP") ? data.TryValue<int>("NDP") : new int?(),
+                Origin = data.ContainsKey("DTO") ? data.TryValue<char>("DTO") : new char?(),
+                SortCode = data.ContainsKey("SCD") ? data.TryValue<int>("SCD") : new int?(),
+                UniqueMultipleResidenceReferenceNumber = data.ContainsKey("UMR") ? data.TryValue<int>("UMR") : new int?(),
+                UniqueDeliveryPointReferenceNumber = data.ContainsKey("URN") ? data.TryValue<int>("URN") : new int?()
             };
 
             return result;
