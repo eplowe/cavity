daf7e28136409c32184a115a48d992018fc2c961
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2011-10-29 09:20:13 +0000

    Re-organising Commands.

---
+++ src/Cavity Commands (File System)/Class Libraries/Commands.FileSystem.Facts/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection;
+
+[assembly: CLSCompliant(true)]
+[assembly: AssemblyDefaultAlias("Cavity.Commands.FileSystem.Facts.dll")]
+[assembly: AssemblyTitle("Cavity.Commands.FileSystem.Facts.dll")]
+
+#if (DEBUG)
+
+[assembly: AssemblyDescription("Cavity : File System Commands Facts Library (Debug)")]
+
+#else
+
+[assembly: AssemblyDescription("Cavity : File System Commands Facts Library (Release)")]
+
+#endif
---
+++ src/Cavity Commands (File System)/Class Libraries/Commands.FileSystem/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+
+[assembly: CLSCompliant(true)]
+[assembly: AssemblyDefaultAlias("Cavity.Commands.FileSystem.dll")]
+[assembly: AssemblyTitle("Cavity.Commands.FileSystem.dll")]
+
+#if (DEBUG)
+
+[assembly: AssemblyDescription("Cavity : File System Commands Library (Debug)")]
+
+#else
+
+[assembly: AssemblyDescription("Cavity : File System Commands Library (Release)")]
+
+#endif
+
+[assembly: SuppressMessage("Microsoft.Design", "CA1020:AvoidNamespacesWithFewTypes", Scope = "namespace", Target = "Cavity")]
