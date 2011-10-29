965dcad41409506201c56fe221b28ae4bfaedf01
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2011-10-29 14:17:29 +0000

    Completed re-organising Commands.

--- trunk/src/Cavity Commands Transactions/Class Libraries/Transactions.Commands.Facts/Transactions/DurableEnlistmentNotification.Derived.cs
+++ trunk/src/Cavity Commands Transactions/Class Libraries/Transactions.Commands.Facts/Transactions/DurableEnlistmentNotification.Derived.cs
@@ -1,19 +1,19 @@
-﻿////namespace Cavity.Transactions
-////{
-////    using System;
-////    using System.Transactions;
+﻿namespace Cavity.Transactions
+{
+    using System;
+    using System.Transactions;
 
-////    public sealed class DerivedDurableEnlistmentNotification : DurableEnlistmentNotification
-////    {
-////        public DerivedDurableEnlistmentNotification(Guid identifier,
-////                                                    EnlistmentOptions enlistmentOptions)
-////            : base(identifier, enlistmentOptions)
-////        {
-////        }
+    public sealed class DerivedDurableEnlistmentNotification : DurableEnlistmentNotification
+    {
+        public DerivedDurableEnlistmentNotification(Guid identifier,
+                                                    EnlistmentOptions enlistmentOptions)
+            : base(identifier, enlistmentOptions)
+        {
+        }
 
-////        protected override bool ConfigureOperation()
-////        {
-////            return true;
-////        }
-////    }
-////}
+        protected override bool ConfigureOperation()
+        {
+            return true;
+        }
+    }
+}
--- trunk/src/Cavity Commands Transactions/Version.cs
+++ trunk/src/Cavity Commands Transactions/Version.cs
@@ -15,7 +15,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.641")]
-[assembly: AssemblyInformationalVersion("1.1.0.641")]
+[assembly: AssemblyFileVersion("1.1.0.642")]
+[assembly: AssemblyInformationalVersion("1.1.0.642")]
 
 
