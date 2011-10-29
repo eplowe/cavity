6ed5cb37399b793d60b7f31e823d8c37e2414ef0
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2011-10-29 14:17:29 +0000

    Completed re-organising Commands.

--- src/Cavity Commands Transactions/Class Libraries/Transactions.Commands.Facts/Transactions/DurableEnlistmentNotification.Facts.cs
+++ src/Cavity Commands Transactions/Class Libraries/Transactions.Commands.Facts/Transactions/DurableEnlistmentNotification.Facts.cs
@@ -1,107 +1,105 @@
-﻿////namespace Cavity.Transactions
-////{
-////    using System;
-////    using System.Diagnostics;
-////    using System.IO;
-////    using System.Threading;
-////    using System.Transactions;
-////    using Cavity.Commands;
-////    using Cavity.IO;
-////    using Xunit;
+﻿namespace Cavity.Transactions
+{
+    using System;
+    using System.IO;
+    using System.Threading;
+    using System.Transactions;
+    using Cavity.IO;
+    using Xunit;
 
-////    public sealed class DurableEnlistmentNotificationFacts
-////    {
-////        [Fact]
-////        public void a_definition()
-////        {
-////            Assert.True(new TypeExpectations<DurableEnlistmentNotification>()
-////                            .DerivesFrom<object>()
-////                            .IsAbstractBaseClass()
-////                            .IsNotDecorated()
-////                            .Result);
-////        }
+    public sealed class DurableEnlistmentNotificationFacts
+    {
+        [Fact]
+        public void a_definition()
+        {
+            Assert.True(new TypeExpectations<DurableEnlistmentNotification>()
+                            .DerivesFrom<object>()
+                            .IsAbstractBaseClass()
+                            .IsNotDecorated()
+                            .Result);
+        }
 
-////#if DEBUG
-////        [Fact]
-////        public void ctor_Guid_EnlistmentOptions()
-////        {
-////            using (new TransactionScope())
-////            {
-////                Assert.NotNull(new DerivedDurableEnlistmentNotification(Guid.NewGuid(), EnlistmentOptions.None));
-////            }
-////        }
-////#endif
+#if DEBUG
+        [Fact]
+        public void ctor_Guid_EnlistmentOptions()
+        {
+            using (new TransactionScope())
+            {
+                Assert.NotNull(new DerivedDurableEnlistmentNotification(Guid.NewGuid(), EnlistmentOptions.None));
+            }
+        }
+#endif
 
-////        [Fact]
-////        public void prop_Operation()
-////        {
-////            Assert.NotNull(new PropertyExpectations<DurableEnlistmentNotification>(p => p.Operation)
-////                               .TypeIs<Operation>()
-////                               .IsNotDecorated()
-////                               .Result);
-////        }
+        [Fact]
+        public void prop_Operation()
+        {
+            Assert.NotNull(new PropertyExpectations<DurableEnlistmentNotification>(p => p.Operation)
+                               .TypeIs<Operation>()
+                               .IsNotDecorated()
+                               .Result);
+        }
 
-////#if DEBUG
-////        [Fact]
-////        [Comment("Transaction: Prepare, Completed, Commit")]
-////        public void transaction_Complete()
-////        {
-////            try
-////            {
-////                using (var temp = new TempDirectory())
-////                {
-////                    var path = temp.Info.ToDirectory("example").FullName;
-////                    Recovery.MasterDirectory = temp.Info.ToDirectory("Recovery");
-////                    using (var scope = new TransactionScope())
-////                    {
-////                        var obj = new DerivedDurableEnlistmentNotification(Guid.NewGuid(), EnlistmentOptions.None);
-////                        obj.Operation.Commands.Add(new DirectoryCreateCommand(path));
+#if DEBUG
+        [Fact]
+        [Comment("Transaction: Prepare, Completed, Commit")]
+        public void transaction_Complete()
+        {
+            try
+            {
+                using (var temp = new TempDirectory())
+                {
+                    var path = temp.Info.ToDirectory("example").FullName;
+                    Recovery.MasterDirectory = temp.Info.ToDirectory("Recovery");
+                    using (var scope = new TransactionScope())
+                    {
+                        var obj = new DerivedDurableEnlistmentNotification(Guid.NewGuid(), EnlistmentOptions.None);
+                        obj.Operation.Commands.Add(new DirectoryCreateCommand(path));
 
-////                        scope.Complete();
-////                    }
+                        scope.Complete();
+                    }
 
-////                    Assert.True(new DirectoryInfo(path).Exists);
-////                    Thread.Sleep(1000);
-////                }
-////            }
-////            finally
-////            {
-////                Recovery.MasterDirectory = null;
-////            }
-////        }
-////#endif
+                    Assert.True(new DirectoryInfo(path).Exists);
+                    Thread.Sleep(1000);
+                }
+            }
+            finally
+            {
+                Recovery.MasterDirectory = null;
+            }
+        }
+#endif
 
-////#if DEBUG
-////        [Fact]
-////        [Comment("Transaction: Completed, Rollback")]
-////        public void transaction_withoutComplete()
-////        {
-////            try
-////            {
-////                using (var temp = new TempDirectory())
-////                {
-////                    var path = temp.Info.ToDirectory("example").FullName;
-////                    Recovery.MasterDirectory = temp.Info.ToDirectory("Recovery");
-////                    using (new TransactionScope())
-////                    {
-////                        var obj = new DerivedDurableEnlistmentNotification(Guid.NewGuid(), EnlistmentOptions.None);
-////                        obj.Operation.Commands.Add(new DirectoryCreateCommand(path));
-////                    }
+#if DEBUG
+        [Fact]
+        [Comment("Transaction: Completed, Rollback")]
+        public void transaction_withoutComplete()
+        {
+            try
+            {
+                using (var temp = new TempDirectory())
+                {
+                    var path = temp.Info.ToDirectory("example").FullName;
+                    Recovery.MasterDirectory = temp.Info.ToDirectory("Recovery");
+                    using (new TransactionScope())
+                    {
+                        var obj = new DerivedDurableEnlistmentNotification(Guid.NewGuid(), EnlistmentOptions.None);
+                        obj.Operation.Commands.Add(new DirectoryCreateCommand(path));
+                    }
 
-////                    Assert.False(new DirectoryInfo(path).Exists);
-////                }
-////            }
-////            finally
-////            {
-////                Recovery.MasterDirectory = null;
-////            }
-////        }
-////#endif
+                    Assert.False(new DirectoryInfo(path).Exists);
+                }
+            }
+            finally
+            {
+                Recovery.MasterDirectory = null;
+            }
+        }
+#endif
 
-////        [Fact]
-////        public void transaction_Missing()
-////        {
-////            Assert.Throws<InvalidOperationException>(() => new DerivedDurableEnlistmentNotification(Guid.NewGuid(), EnlistmentOptions.None));
-////        }
-////    }
-////}
+        [Fact]
+        public void transaction_Missing()
+        {
+            Assert.Throws<InvalidOperationException>(() => new DerivedDurableEnlistmentNotification(Guid.NewGuid(), EnlistmentOptions.None));
+        }
+    }
+}
