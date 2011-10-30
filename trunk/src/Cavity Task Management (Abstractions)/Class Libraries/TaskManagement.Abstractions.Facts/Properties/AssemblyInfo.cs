afbc15e8c09353f1a23b938ab3cab29c1086ea79
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2011-10-30 13:25:20 +0000

    Bringing Task Management back from it's separate repo.

---
+++ trunk/src/Cavity Task Management (Abstractions)/Class Libraries/TaskManagement.Abstractions.Facts/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection;
+
+[assembly: CLSCompliant(true)]
+[assembly: AssemblyDefaultAlias("Cavity.TaskManagement.Abstractions.Facts.dll")]
+[assembly: AssemblyTitle("Cavity.TaskManagement.Abstractions.Facts.dll")]
+
+#if (DEBUG)
+
+[assembly: AssemblyDescription("Cavity : Task Management Abstractions Facts Library (Debug)")]
+
+#else
+
+[assembly: AssemblyDescription("Cavity : Task Management Abstractions Facts Library (Release)")]
+
+#endif
---
+++ trunk/src/Cavity Task Management/Class Libraries/TaskManagement.Facts/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection;
+
+[assembly: CLSCompliant(true)]
+[assembly: AssemblyDefaultAlias("Cavity.TaskManagement.Facts.dll")]
+[assembly: AssemblyTitle("Cavity.TaskManagement.Facts.dll")]
+
+#if (DEBUG)
+
+[assembly: AssemblyDescription("Cavity : Task Management Facts Library (Debug)")]
+
+#else
+
+[assembly: AssemblyDescription("Cavity : Task Management Facts Library (Release)")]
+
+#endif
