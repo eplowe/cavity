d733ca5326e39fc82352144ef48a58befe81f62f
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2011-10-30 21:13:11 +0000

    Bringing Task Management back from it's separate repo.

---
+++ trunk/src/Cavity Task Management Service/Windows Services/TaskManagement.Service/ServiceInstallation.cs
@@ -0,0 +1,14 @@
+﻿namespace Cavity
+{
+    using System.ComponentModel;
+    using System.Configuration.Install;
+
+    [RunInstaller(true)]
+    public partial class ServiceInstallation : Installer
+    {
+        public ServiceInstallation()
+        {
+            InitializeComponent();
+        }
+    }
+}
