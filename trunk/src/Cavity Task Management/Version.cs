6728c011b53ab9bc767a2898dceca0bcaea9c537
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2011-11-05 16:03:32 +0000

    Tweaked FileReceiver<T> to listen to subordinate directories as well.

--- trunk/src/Cavity Task Management/Version.cs
+++ trunk/src/Cavity Task Management/Version.cs
@@ -15,7 +15,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.694")]
-[assembly: AssemblyInformationalVersion("1.1.0.694")]
+[assembly: AssemblyFileVersion("1.1.0.711")]
+[assembly: AssemblyInformationalVersion("1.1.0.711")]
 
 
