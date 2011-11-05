09c8fb120685d1ae351736fa78e9f0dc93e58c28
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2011-11-05 19:45:49 +0000

    Implemented Test Controller Abstractions.

---
+++ src/Cavity Test Controller Abstractions/Class Libraries/TestController.Abstractions.Facts/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection;
+
+[assembly: CLSCompliant(true)]
+[assembly: AssemblyDefaultAlias("Cavity.TestController.Abstractions.Facts.dll")]
+[assembly: AssemblyTitle("Cavity.TestController.Abstractions.Facts.dll")]
+
+#if (DEBUG)
+
+[assembly: AssemblyDescription("Cavity : Test Controller Abstractions Facts Library (Debug)")]
+
+#else
+
+[assembly: AssemblyDescription("Cavity : Test Controller Abstractions Facts Library (Release)")]
+
+#endif
