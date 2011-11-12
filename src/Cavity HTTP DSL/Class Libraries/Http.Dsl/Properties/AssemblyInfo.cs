5aae918939821b94eb6d31547d68115eee514d49
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2011-11-12 18:37:20 +0000

    New HTTP DSL solution.

---
+++ src/Cavity HTTP DSL/Class Libraries/Http.Dsl/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+
+[assembly: CLSCompliant(true)]
+[assembly: AssemblyDefaultAlias("Cavity.Http.Dsl.dll")]
+[assembly: AssemblyTitle("Cavity.Http.Dsl.dll")]
+
+#if (DEBUG)
+
+[assembly: AssemblyDescription("Cavity : HTTP DSL Library (Debug)")]
+
+#else
+
+[assembly: AssemblyDescription("Cavity : HTTP DSL Library (Release)")]
+
+#endif
+
+[assembly: SuppressMessage("Microsoft.Design", "CA1020:AvoidNamespacesWithFewTypes", Scope = "namespace", Target = "Cavity")]
