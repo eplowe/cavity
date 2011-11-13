51b2d8fe123b1828a9b911038bdd5c22729e9cef
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2011-11-13 19:47:50 +0000

    New HTTP Testing solution.

---
+++ src/Cavity HTTP Testing/Class Libraries/Http.Testing.Facts/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+
+[assembly: CLSCompliant(true)]
+[assembly: AssemblyDefaultAlias("Cavity.Http.Testing.Facts.dll")]
+[assembly: AssemblyTitle("Cavity.Http.Testing.Facts.dll")]
+
+#if (DEBUG)
+
+[assembly: AssemblyDescription("Cavity : HTTP Testing Facts Library (Debug)")]
+
+#else
+
+[assembly: AssemblyDescription("Cavity : HTTP Testing Facts Library (Release)")]
+
+#endif
+
+[assembly: SuppressMessage("Microsoft.Design", "CA1020:AvoidNamespacesWithFewTypes", Scope = "namespace", Target = "Cavity")]
