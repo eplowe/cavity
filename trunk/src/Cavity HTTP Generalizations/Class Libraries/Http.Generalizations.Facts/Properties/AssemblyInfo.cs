342e898d8b85299b7285529a62da440d5593c019
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2011-11-13 19:14:51 +0000

    New HTTP Generalizations solution.

---
+++ trunk/src/Cavity HTTP Generalizations/Class Libraries/Http.Generalizations.Facts/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+
+[assembly: CLSCompliant(true)]
+[assembly: AssemblyDefaultAlias("Cavity.Http.Generalizations.Facts.dll")]
+[assembly: AssemblyTitle("Cavity.Http.Generalizations.Facts.dll")]
+
+#if (DEBUG)
+
+[assembly: AssemblyDescription("Cavity : HTTP Generalizations Facts Library (Debug)")]
+
+#else
+
+[assembly: AssemblyDescription("Cavity : HTTP Generalizations Facts Library (Release)")]
+
+#endif
+
+[assembly: SuppressMessage("Microsoft.Design", "CA1020:AvoidNamespacesWithFewTypes", Scope = "namespace", Target = "Cavity")]
---
+++ trunk/src/Cavity HTTP Generalizations/Class Libraries/Http.Generalizations/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+
+[assembly: CLSCompliant(true)]
+[assembly: AssemblyDefaultAlias("Cavity.Http.Generalizations.dll")]
+[assembly: AssemblyTitle("Cavity.Http.Generalizations.dll")]
+
+#if (DEBUG)
+
+[assembly: AssemblyDescription("Cavity : HTTP Generalizations Library (Debug)")]
+
+#else
+
+[assembly: AssemblyDescription("Cavity : HTTP Generalizations Library (Release)")]
+
+#endif
+
+[assembly: SuppressMessage("Microsoft.Design", "CA1020:AvoidNamespacesWithFewTypes", Scope = "namespace", Target = "Cavity")]
