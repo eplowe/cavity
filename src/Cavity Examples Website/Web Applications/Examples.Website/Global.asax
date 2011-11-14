5df067e724a1946576376180b544bbc8cf0711f3
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2011-11-14 22:14:19 +0000

    Fixing nuget packages of log4net and service location to support web as well as console.

---
+++ src/Cavity Examples Website/Web Applications/Examples.Website/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Cavity.MvcApplication1" Language="C#" %>
---
+++ src/Cavity Examples Website/Web Applications/Examples.Website/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+
+[assembly: CLSCompliant(true)]
+[assembly: AssemblyDefaultAlias("Cavity.Examples.Website.dll")]
+[assembly: AssemblyTitle("Cavity.Examples.Website.dll")]
+
+#if (DEBUG)
+
+[assembly: AssemblyDescription("Cavity : Examples Website Library (Debug)")]
+
+#else
+
+[assembly: AssemblyDescription("Cavity : Examples Website Library (Release)")]
+
+#endif
+
+[assembly: SuppressMessage("Microsoft.Design", "CA1020:AvoidNamespacesWithFewTypes", Scope = "namespace", Target = "Cavity")]
+[assembly: SuppressMessage("Microsoft.Design", "CA2210:AssembliesShouldHaveValidStrongNames", Justification = "There are unsigned dependencies.")]
