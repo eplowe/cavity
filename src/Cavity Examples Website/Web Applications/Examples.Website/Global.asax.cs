098490a271c7a54072d52d67436c365786c2c29c
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2011-11-19 11:32:13 +0000

    Added setup project to examples website.

--- src/Cavity Examples Website/Web Applications/Examples.Website/Global.asax.cs
+++ src/Cavity Examples Website/Web Applications/Examples.Website/Global.asax.cs
@@ -1,10 +1,12 @@
 ﻿namespace Cavity
 {
     using System;
+    using System.Diagnostics;
     using System.Diagnostics.CodeAnalysis;
     using System.Web;
     using System.Web.Mvc;
     using System.Web.Routing;
+    using Cavity.Diagnostics;
     using Cavity.Web.Mvc;
     using Cavity.Web.Routing;
 
@@ -12,6 +14,8 @@
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
+            Trace.WriteLineIf(Tracing.Is.TraceVerbose, string.Empty);
+
             if (null == filters)
             {
                 throw new ArgumentNullException("filters");
@@ -22,6 +26,8 @@
 
         public static void RegisterRoutes(RouteCollection routes)
         {
+            Trace.WriteLineIf(Tracing.Is.TraceVerbose, string.Empty);
+
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.Register(typeof(MvcApplication).Assembly.GetTypes());
@@ -31,6 +37,8 @@
         [SuppressMessage("Microsoft.Performance", "CA1822:MarkMembersAsStatic", Justification = "The runtime requires this to be an instance member.")]
         protected void Application_Start()
         {
+            Trace.WriteLineIf(Tracing.Is.TraceVerbose, string.Empty);
+
             AreaRegistration.RegisterAllAreas();
 
             RegisterGlobalFilters(GlobalFilters.Filters);
