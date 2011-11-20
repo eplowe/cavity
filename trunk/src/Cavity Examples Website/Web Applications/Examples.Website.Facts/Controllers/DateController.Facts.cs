8491800ae8a8c6cae7d5ae47731f097f98a82455
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2011-11-20 23:08:20 +0000

    Working on HTTP testing.

---
+++ trunk/src/Cavity Examples Website/Web Applications/Examples.Website.Facts/Controllers/DateController.Facts.cs
@@ -0,0 +1,62 @@
+﻿namespace Cavity.Controllers
+{
+    using System;
+    using System.Linq;
+    using System.Web.Mvc;
+    using System.Web.Routing;
+    using Cavity;
+    using Cavity.Globalization;
+    using Cavity.Web.Mvc;
+    using Cavity.Web.Routing;
+    using Xunit;
+
+    public sealed class DateControllerFacts
+    {
+        [Fact]
+        public void a_definition()
+        {
+            Assert.True(new TypeExpectations<DateController>()
+                            .DerivesFrom<LanguageController>()
+                            .IsConcreteClass()
+                            .IsSealed()
+                            .HasDefaultConstructor()
+                            .IsDecoratedWith<AllowAttribute>()
+                            .Implements<IRegisterRoutes>()
+                            .Result);
+        }
+
+        [Fact]
+        public void ctor()
+        {
+            Assert.NotNull(new DateController());
+        }
+
+        [Fact]
+        public void op_RegisterRoutes_RouteCollectionNull()
+        {
+            var controller = (IRegisterRoutes)new DateController();
+
+            Assert.Throws<ArgumentNullException>(() => controller.Register(null));
+        }
+
+        [Fact]
+        public void op_RegisterRoutes_RouteCollection()
+        {
+            var routes = new RouteCollection();
+
+            var controller = (IRegisterRoutes)new DateController();
+            controller.Register(routes);
+
+            Assert.Equal("today", ((Route)routes["Today (lanneg)"]).Url);
+            Assert.Equal("today.{language}", ((Route)routes["Today (conneg)"]).Url);
+            Assert.Equal("today.{language}.html", ((Route)routes["Today"]).Url);
+        }
+
+        [Fact]
+        public void prop_Languages()
+        {
+            Assert.Equal<Language>("en", new DateController().Languages.First());
+            Assert.Equal<Language>("fr", new DateController().Languages.Last());
+        }
+    }
+}
---
+++ trunk/src/Cavity Examples Website/Web Applications/Examples.Website/Controllers/EchoController.cs
@@ -0,0 +1,100 @@
+﻿namespace Cavity.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Web.Caching;
+    using System.Web.Mvc;
+    using System.Web.Routing;
+    using Cavity.Globalization;
+    using Cavity.Web.Mvc;
+    using Cavity.Web.Routing;
+
+    [Allow("GET, HEAD, OPTIONS, POST")]
+    public sealed class EchoController : LanguageController, IRegisterRoutes
+    {
+        public override IEnumerable<Language> Languages
+        {
+            get
+            {
+                yield return "en";
+                yield return "fr";
+            }
+        }
+
+        [ContentNegotiation(".html", "*/*, text/*, text/html")]
+        public ActionResult HtmlRepresentation(CultureInfo language, string key)
+        {
+            language.SetCurrentCulture();
+
+            ViewBag.Key = key;
+
+            return View(HttpContext.Cache[key]);
+        }
+
+        public ActionResult Post()
+        {
+            var key = AlphaDecimal.Random();
+
+            HttpContext.Cache.Add(key, 
+                                  Request.Form, 
+                                  null, 
+                                  DateTime.UtcNow.AddMinutes(30), 
+                                  Cache.NoSlidingExpiration, 
+                                  CacheItemPriority.Default, 
+                                  null);
+
+            return new SeeOtherResult("/echo/{0}".FormatWith(key));
+        }
+
+        void IRegisterRoutes.Register(RouteCollection routes)
+        {
+            if (null == routes)
+            {
+                throw new ArgumentNullException("routes");
+            }
+
+            routes.MapRoute(
+                "Echo (POST)", 
+                "echo", 
+                new
+                {
+                    controller = "Echo", 
+                    action = "Post"
+                }, 
+                new
+                {
+                    method = new HttpMethodConstraint("POST")
+                });
+
+            routes.MapRoute(
+                "Echo (HTML)",
+                "echo/{key}.{language}.html",
+                new
+                {
+                    controller = "Echo",
+                    action = "HtmlRepresentation",
+                    language = "fr"
+                });
+
+            routes.MapRoute(
+                "Echo (conneg)",
+                "echo/{key}.{language}",
+                new
+                {
+                    controller = "Echo",
+                    action = "ContentNegotiation",
+                    language = "fr"
+                });
+
+            routes.MapRoute(
+                "Echo (lanneg)",
+                "echo/{key}",
+                new
+                {
+                    controller = "Echo",
+                    action = "LanguageNegotiation"
+                });
+        }
+    }
+}
