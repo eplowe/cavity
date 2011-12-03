ef3e82221e68fa2c205709a4890a98ddd1ef7cb0
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2011-12-03 09:57:43 +0000

    Fixing postcode bug where area-only displays as a district.

--- trunk/src/Cavity Domain (Royal Mail)/Version.cs
+++ trunk/src/Cavity Domain (Royal Mail)/Version.cs
@@ -15,7 +15,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.757")]
-[assembly: AssemblyInformationalVersion("1.1.0.757")]
+[assembly: AssemblyFileVersion("1.1.0.758")]
+[assembly: AssemblyInformationalVersion("1.1.0.758")]
 
 
