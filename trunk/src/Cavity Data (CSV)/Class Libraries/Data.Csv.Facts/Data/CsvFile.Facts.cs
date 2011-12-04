220ee120734a562a1af5c503a41a92181d61ff60
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2011-12-04 10:31:20 +0000

    Improved support for CsvFile.Line(...) formatting support by optionally specifying headers.

--- trunk/src/Cavity Data (CSV)/Class Libraries/Data.Csv.Facts/Data/CsvFile.Facts.cs
+++ trunk/src/Cavity Data (CSV)/Class Libraries/Data.Csv.Facts/Data/CsvFile.Facts.cs
@@ -75,26 +75,11 @@
         {
             var obj = new KeyStringDictionary
             {
-                new KeyStringPair("A,B", string.Empty),
+                new KeyStringPair("A,B", string.Empty), 
                 new KeyStringPair("C", string.Empty)
             };
 
             const string expected = "\"A,B\",C";
-            var actual = CsvFile.Header(obj);
-
-            Assert.Equal(expected, actual);
-        }
-
-        [Fact]
-        public void op_Header_KeyStringDictionary_whenEmptyValue()
-        {
-            var obj = new KeyStringDictionary
-            {
-                new KeyStringPair(string.Empty, "x"),
-                new KeyStringPair("A,B", "x")
-            };
-
-            const string expected = ",\"A,B\"";
             var actual = CsvFile.Header(obj);
 
             Assert.Equal(expected, actual);
@@ -113,6 +98,21 @@
         }
 
         [Fact]
+        public void op_Header_KeyStringDictionary_whenEmptyValue()
+        {
+            var obj = new KeyStringDictionary
+            {
+                new KeyStringPair(string.Empty, "x"), 
+                new KeyStringPair("A,B", "x")
+            };
+
+            const string expected = ",\"A,B\"";
+            var actual = CsvFile.Header(obj);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
         public void op_IEnumerable_GetEnumerator()
         {
             using (var file = new TempFile())
@@ -128,72 +128,15 @@
         }
 
         [Fact]
-        public void op_Line_KeyStringDictionary()
-        {
-            var obj = new KeyStringDictionary
-            {
-                new KeyStringPair("A", "123"),
-                new KeyStringPair("B", "left,right")
-            };
-
-            const string expected = "123,\"left,right\"";
-            var actual = CsvFile.Line(obj);
-
-            Assert.Equal(expected, actual);
-        }
-
-        [Fact]
-        public void op_Line_KeyStringDictionary_whenEmptyValue()
-        {
-            var obj = new KeyStringDictionary
-            {
-                new KeyStringPair("A", string.Empty),
-                new KeyStringPair("B", "left,right")
-            };
-
-            const string expected = ",\"left,right\"";
-            var actual = CsvFile.Line(obj);
-
-            Assert.Equal(expected, actual);
-        }
-
-        [Fact]
-        public void op_Line_KeyStringDictionaryEmpty()
-        {
-            Assert.Throws<ArgumentOutOfRangeException>(() => CsvFile.Line(new KeyStringDictionary()));
-        }
-
-        [Fact]
-        public void op_Line_KeyStringDictionaryNull()
-        {
-            Assert.Throws<ArgumentNullException>(() => CsvFile.Line(null as KeyStringDictionary));
-        }
-
-        [Fact]
         public void op_Line_IEnumerableString()
         {
             var obj = new List<string>
             {
-                "123",
+                "123", 
                 "left,right"
             };
 
             const string expected = "123,\"left,right\"";
-            var actual = CsvFile.Line(obj);
-
-            Assert.Equal(expected, actual);
-        }
-
-        [Fact]
-        public void op_Line_IEnumerableString_whenEmptyValue()
-        {
-            var obj = new List<string>
-            {
-                string.Empty,
-                "left,right"
-            };
-
-            const string expected = ",\"left,right\"";
             var actual = CsvFile.Line(obj);
 
             Assert.Equal(expected, actual);
@@ -212,13 +155,126 @@
         }
 
         [Fact]
+        public void op_Line_IEnumerableString_whenEmptyValue()
+        {
+            var obj = new List<string>
+            {
+                string.Empty, 
+                "left,right"
+            };
+
+            const string expected = ",\"left,right\"";
+            var actual = CsvFile.Line(obj);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void op_Line_KeyStringDictionary()
+        {
+            var obj = new KeyStringDictionary
+            {
+                new KeyStringPair("A", "123"), 
+                new KeyStringPair("B", "left,right")
+            };
+
+            const string expected = "123,\"left,right\"";
+            var actual = CsvFile.Line(obj);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void op_Line_KeyStringDictionaryEmpty()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => CsvFile.Line(new KeyStringDictionary()));
+        }
+
+        [Fact]
+        public void op_Line_KeyStringDictionaryNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => CsvFile.Line(null as KeyStringDictionary));
+        }
+
+        [Fact]
+        public void op_Line_KeyStringDictionary_string()
+        {
+            var obj = new KeyStringDictionary
+            {
+                new KeyStringPair("A", "123"), 
+                new KeyStringPair("B", "ignore"), 
+                new KeyStringPair("C", "left,right")
+            };
+
+            const string expected = "123,\"left,right\"";
+            var actual = CsvFile.Line(obj, "A,C");
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void op_Line_KeyStringDictionary_stringEmpty()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => CsvFile.Line(new KeyStringDictionary(), string.Empty));
+        }
+
+        [Fact]
+        public void op_Line_KeyStringDictionary_stringNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => CsvFile.Line(new KeyStringDictionary(), null as string));
+        }
+
+        [Fact]
+        public void op_Line_KeyStringDictionary_string_whenColumnsNotFound()
+        {
+            Assert.Throws<KeyNotFoundException>(() => CsvFile.Line(new KeyStringDictionary(), "A"));
+        }
+
+        [Fact]
+        public void op_Line_KeyStringDictionary_strings()
+        {
+            var obj = new KeyStringDictionary
+            {
+                new KeyStringPair("A", "123"), 
+                new KeyStringPair("B", "ignore"), 
+                new KeyStringPair("C", "left,right")
+            };
+
+            const string expected = "123,\"left,right\"";
+            var actual = CsvFile.Line(obj, "A", "C");
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void op_Line_KeyStringDictionary_strings_whenColumnsNotFound()
+        {
+            Assert.Throws<KeyNotFoundException>(() => CsvFile.Line(new KeyStringDictionary(), "A", "B"));
+        }
+
+        [Fact]
+        public void op_Line_KeyStringDictionary_whenEmptyValue()
+        {
+            var obj = new KeyStringDictionary
+            {
+                new KeyStringPair("A", string.Empty), 
+                new KeyStringPair("B", "left,right")
+            };
+
+            const string expected = ",\"left,right\"";
+            var actual = CsvFile.Line(obj);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
         public void op_Save_FileMode_IEnumerableKeyStringDictionary()
         {
             var data = new[]
             {
                 new KeyStringDictionary
                 {
-                    new KeyStringPair("A", "1"),
+                    new KeyStringPair("A", "1"), 
                     new KeyStringPair("B", "2")
                 }
             };
@@ -272,7 +328,7 @@
                     {
                         file, new KeyStringDictionary
                         {
-                            new KeyStringPair("A", "1"),
+                            new KeyStringPair("A", "1"), 
                             new KeyStringPair("B", "2")
                         }
                         }
