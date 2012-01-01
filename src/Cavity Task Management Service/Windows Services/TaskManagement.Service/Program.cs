c833b0be715383c744891907e01aeb75786c196d
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2012-01-01 17:50:13 +0000

    Corrected Main after nuget update.

--- src/Cavity Task Management Service/Windows Services/TaskManagement.Service/Program.cs
+++ src/Cavity Task Management Service/Windows Services/TaskManagement.Service/Program.cs
@@ -10,8 +10,6 @@
     {
         public static void Main()
         {
-            log4net.Config.XmlConfigurator.Configure();
-            Config.ExeSection<ServiceLocation>().Provider.Configure();
             try
             {
                 log4net.Config.XmlConfigurator.Configure();
