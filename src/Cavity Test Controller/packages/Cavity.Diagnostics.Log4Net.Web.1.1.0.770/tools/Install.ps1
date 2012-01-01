da05f0bf8dc81be499820b1f626ab43646b171d9
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2012-01-01 22:42:17 +0000

    Implemented nuget package restore across all projects.

--- src/Cavity Test Controller/packages/Cavity.Diagnostics.Log4Net.Web.1.1.0.770/tools/Install.ps1
+++
@@ -1,3 +0,0 @@
-param($installPath, $toolsPath, $package, $project)
-
-$project.ProjectItems.Item("log4net.config").Properties.Item("CopyToOutputDirectory").Value = 1
--- src/Cavity Web (ASP.NET MVC3)/Version.cs
+++ src/Cavity Web (ASP.NET MVC3)/Version.cs
@@ -15,7 +15,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.772")]
-[assembly: AssemblyInformationalVersion("1.1.0.772")]
+[assembly: AssemblyFileVersion("1.1.0.773")]
+[assembly: AssemblyInformationalVersion("1.1.0.773")]
 
 
