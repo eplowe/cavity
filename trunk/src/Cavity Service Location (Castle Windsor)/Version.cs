e0419c76b00da1012531142ef38b1487edcd36a6
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2012-04-08 17:29:40 +0000

    Fixed VS Test Library template.

--- trunk/src/Cavity Service Location (Castle Windsor)/Version.cs
+++ trunk/src/Cavity Service Location (Castle Windsor)/Version.cs
@@ -15,7 +15,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.799")]
-[assembly: AssemblyInformationalVersion("1.1.0.799")]
+[assembly: AssemblyFileVersion("1.1.0.800")]
+[assembly: AssemblyInformationalVersion("1.1.0.800")]
 
 
