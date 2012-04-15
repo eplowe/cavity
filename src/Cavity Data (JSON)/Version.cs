c6e7ec030e52dc6bec8b6128a0fdf7091653399a
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2012-04-15 21:54:03 +0000

    Moved Example.cs into the Facts library.

--- src/Cavity Data (JSON)/Version.cs
+++ src/Cavity Data (JSON)/Version.cs
@@ -15,7 +15,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.801")]
-[assembly: AssemblyInformationalVersion("1.1.0.801")]
+[assembly: AssemblyFileVersion("1.1.0.802")]
+[assembly: AssemblyInformationalVersion("1.1.0.802")]
 
 
