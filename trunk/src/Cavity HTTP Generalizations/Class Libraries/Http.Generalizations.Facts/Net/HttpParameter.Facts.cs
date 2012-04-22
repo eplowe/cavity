52a8a491940e641c2f75f859ab09d632881646c9
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2012-04-22 16:54:23 +0000

    Working on HTTP libraries.

---
+++ trunk/src/Cavity HTTP Generalizations/Class Libraries/Http.Generalizations.Facts/Net/HttpParameter.Facts.cs
@@ -0,0 +1,142 @@
+﻿namespace Cavity.Net
+{
+    using System;
+
+    using Xunit;
+    using Xunit.Extensions;
+
+    public sealed class ParameterFacts
+    {
+        [Fact]
+        public void a_definition()
+        {
+            Assert.True(new TypeExpectations<Parameter>()
+                            .DerivesFrom<ComparableObject>()
+                            .IsConcreteClass()
+                            .IsUnsealed()
+                            .NoDefaultConstructor()
+                            .IsNotDecorated()
+                            .Result);
+        }
+
+        [Fact]
+        public void ctor_Token_string()
+        {
+            Assert.NotNull(new Parameter("name", "value"));
+        }
+
+        [Fact]
+        public void opImplicit_Parameter_string()
+        {
+            const string expected = "name=value";
+
+            Parameter obj = expected;
+
+            var actual = obj.ToString();
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void opImplicit_string_Parameter()
+        {
+            const string expected = "name=value";
+
+            var obj = Parameter.FromString(expected);
+
+            string actual = obj;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void op_FromString_string()
+        {
+            const string expected = "name=value";
+
+            var obj = Parameter.FromString(expected);
+
+            var actual = obj.ToString();
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("   ")]
+        public void op_FromString_stringEmpty(string value)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => Parameter.FromString(value));
+        }
+
+        [Fact]
+        public void op_FromString_stringNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => Parameter.FromString(null));
+        }
+
+        [Fact]
+        public void op_FromString_string_whenEmptyName()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => Parameter.FromString("=value"));
+        }
+
+        [Fact]
+        public void op_FromString_string_whenEmptyValue()
+        {
+            const string expected = "name=";
+
+            var obj = Parameter.FromString(expected);
+
+            var actual = obj.ToString();
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void op_FromString_string_whenTwoEquals()
+        {
+            Assert.Throws<FormatException>(() => Parameter.FromString("name=value=example"));
+        }
+
+        [Fact]
+        public void op_FromString_string_withSemicolon()
+        {
+            Assert.Throws<FormatException>(() => Parameter.FromString(";example"));
+        }
+
+        [Fact]
+        public void op_FromString_string_withoutEquals()
+        {
+            Assert.Throws<FormatException>(() => Parameter.FromString("example"));
+        }
+
+        [Fact]
+        public void op_ToString()
+        {
+            var obj = new Parameter("name", "value");
+            const string expected = "name=value";
+            var actual = obj.ToString();
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void prop_Name()
+        {
+            Assert.True(new PropertyExpectations<Parameter>(x => x.Name)
+                            .IsAutoProperty<Token>()
+                            .IsNotDecorated()
+                            .Result);
+        }
+
+        [Fact]
+        public void prop_Value()
+        {
+            Assert.True(new PropertyExpectations<Parameter>(x => x.Value)
+                            .IsAutoProperty<string>()
+                            .IsNotDecorated()
+                            .Result);
+        }
+    }
+}
---
+++ trunk/src/Cavity HTTP Generalizations/Class Libraries/Http.Generalizations/Net/HttpParameter.cs
@@ -0,0 +1,69 @@
+﻿namespace Cavity.Net
+{
+    using System;
+
+    using Cavity.Properties;
+
+    public class Parameter : ComparableObject
+    {
+        public Parameter(Token name, string value)
+            : this()
+        {
+            Name = name;
+            Value = value;
+        }
+
+        private Parameter()
+        {
+        }
+
+        public Token Name { get; set; }
+
+        public string Value { get; set; }
+
+        public static implicit operator Parameter(string value)
+        {
+            return (null == value) ? null : FromString(value);
+        }
+
+        public static Parameter FromString(string value)
+        {
+            if (null == value)
+            {
+                throw new ArgumentNullException("value");
+            }
+
+            value = value.Trim();
+            if (0 == value.Length)
+            {
+                throw new ArgumentOutOfRangeException("value");
+            }
+
+            if (value.ContainsAny(';'))
+            {
+                throw new FormatException(Resources.Parameter_FormatException_MessageSemicolon);
+            }
+
+#if NET20
+            var parts = StringExtensionMethods.Split(value, '=', StringSplitOptions.None);
+#else
+            var parts = value.Split('=', StringSplitOptions.None);
+#endif
+            if (2 != parts.Length)
+            {
+                throw new FormatException(Resources.Parameter_FormatException_Message);
+            }
+
+            return new Parameter(parts[0], parts[1]);
+        }
+
+        public override string ToString()
+        {
+#if NET20
+            return StringExtensionMethods.FormatWith("{0}={1}", Name, Value);
+#else
+            return "{0}={1}".FormatWith(Name, Value);
+#endif
+        }
+    }
+}
