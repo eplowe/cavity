faa5dcd735de3bc63e8b123f6da47502f27ea1e1
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2012-04-23 19:28:47 +0000

    Working on HTTP libraries.

---
+++ trunk/src/Cavity HTTP Generalizations/Class Libraries/Http.Generalizations.Facts/Net/IHttpClient.Facts.cs
@@ -0,0 +1,71 @@
+﻿namespace Cavity.Net
+{
+    using System;
+    using System.Net;
+
+    using Cavity;
+
+    using Moq;
+
+    using Xunit;
+
+    public sealed class IHttpClientFacts
+    {
+        [Fact]
+        public void a_definition()
+        {
+            Assert.True(new TypeExpectations<IHttpClient>()
+                            .IsInterface()
+                            .IsNotDecorated()
+                            .Implements<IDisposable>()
+                            .Result);
+        }
+
+        [Fact]
+        public void prop_AutoRedirect_get()
+        {
+            const bool expected = true;
+            var mock = new Mock<IHttpClient>();
+            mock
+                .Setup(x => x.AutoRedirect)
+                .Returns(expected)
+                .Verifiable();
+
+            var actual = mock.Object.AutoRedirect;
+
+            Assert.Equal(expected, actual);
+
+            mock.VerifyAll();
+        }
+
+        [Fact]
+        public void prop_AutoRedirect_set()
+        {
+            var mock = new Mock<IHttpClient>();
+            mock
+                .SetupSet(x => x.AutoRedirect = false)
+                .Verifiable();
+
+            mock.Object.AutoRedirect = false;
+
+            mock.VerifyAll();
+        }
+
+        [Fact]
+        public void prop_CookieContainer_get()
+        {
+            var expected = new CookieContainer();
+            var mock = new Mock<IHttpClient>();
+            mock
+                .Setup(x => x.Cookies)
+                .Returns(expected)
+                .Verifiable();
+
+            var actual = mock.Object.Cookies;
+
+            Assert.Same(expected, actual);
+
+            mock.VerifyAll();
+        }
+    }
+}
