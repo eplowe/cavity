935f35416abd4f42f57e77c8f0ec74a58198419d
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2012-04-23 06:07:04 +0000

    Restructured HTML assemblies.

---
+++ src/Cavity Data (HTML)/Class Libraries/Data.Html.Facts/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+
+[assembly: CLSCompliant(false)]
+[assembly: AssemblyDefaultAlias("Cavity.Data.Html.Facts.dll")]
+[assembly: AssemblyTitle("Cavity.Data.Html.Facts.dll")]
+
+#if (DEBUG)
+
+[assembly: AssemblyDescription("Cavity : Data HTML Facts Library (Debug)")]
+
+#else
+
+[assembly: AssemblyDescription("Cavity : Data HTML Facts Library (Release)")]
+
+#endif
+
+[assembly: SuppressMessage("Microsoft.Design", "CA1020:AvoidNamespacesWithFewTypes", Scope = "namespace", Target = "Cavity")]
---
+++ src/Cavity Data (HTML)/Class Libraries/Data.Html/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+
+[assembly: CLSCompliant(false)]
+[assembly: AssemblyDefaultAlias("Cavity.Data.Html.dll")]
+[assembly: AssemblyTitle("Cavity.Data.Html.dll")]
+
+#if (DEBUG)
+
+[assembly: AssemblyDescription("Cavity : Data HTML Library (Debug)")]
+
+#else
+
+[assembly: AssemblyDescription("Cavity : Data HTML Library (Release)")]
+
+#endif
+
+[assembly: SuppressMessage("Microsoft.Design", "CA1020:AvoidNamespacesWithFewTypes", Scope = "namespace", Target = "Cavity")]
