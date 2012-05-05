03b91720eaf4d6131e31030c4965fd53bfd346ff
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2012-05-05 13:57:17 +0000

    Started work on version control console app.

---
+++ src/Cavity Version Control/Console Applications/Version/CommandLine.cs
@@ -0,0 +1,97 @@
+﻿namespace Cavity
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Reflection;
+    using System.Security;
+#if NET20 || NET35
+    using System.Security.Permissions;
+#endif
+
+    using Cavity.Properties;
+
+    public sealed class CommandLine
+    {
+        private CommandLine(IEnumerable<string> parameters)
+        {
+            Parameters = parameters;
+        }
+
+        public bool Help
+        {
+            get
+            {
+                return null != this["?"];
+            }
+        }
+
+        public bool Info
+        {
+            get
+            {
+                return null != this["i"];
+            }
+        }
+
+        public IEnumerable<string> Parameters { get; private set; }
+
+        public string this[string key]
+        {
+            get
+            {
+                foreach (var parameter in Parameters)
+                {
+                    if (string.Equals(parameter, string.Concat('/', key), StringComparison.OrdinalIgnoreCase))
+                    {
+                        return string.Empty;
+                    }
+
+                    if (parameter.StartsWith(string.Concat('/', key, ':'), StringComparison.OrdinalIgnoreCase))
+                    {
+                        return parameter.Substring(parameter.IndexOf(':'));
+                    }
+                }
+
+                return null;
+            }
+        }
+
+#if NET20 || NET35
+        [SecurityPermission(SecurityAction.LinkDemand, UnmanagedCode = true)]
+#else
+        [SuppressMessage("Microsoft.Security", "CA2122:DoNotIndirectlyExposeMethodsWithLinkDemands", Justification = "CA2122 rule has not been fully updated to work with level 2 transparency.")]
+        [SecurityCritical]
+#endif
+        public static CommandLine Load(ICollection<string> args)
+        {
+            var result = new CommandLine(args);
+
+            var assembly = Assembly.GetExecutingAssembly();
+            var info = FileVersionInfo.GetVersionInfo(assembly.Location).ProductVersion;
+            Console.WriteLine(Resources.Splash, info);
+            if (null == args)
+            {
+                return result;
+            }
+
+            if (!result.Help)
+            {
+                Console.WriteLine(string.Empty);
+                Console.WriteLine(Resources.Usage);
+                Console.WriteLine(string.Empty);
+                Console.WriteLine(Resources.UsageDetails);
+                Console.WriteLine(string.Empty);
+                Console.WriteLine(Resources.Parameters);
+                Console.WriteLine(string.Empty);
+                Console.WriteLine(Resources.ParametersHelp);
+                Console.WriteLine(Resources.ParametersInfo);
+                Console.WriteLine(string.Empty);
+                Console.WriteLine(Resources.HelpLink);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/Cavity Version Control/Console Applications/Version/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Cavity
+{
+    using System;
+
+    using Cavity.Models;
+
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Process(CommandLine.Load(args));
+        }
+
+        public static void Process(CommandLine args)
+        {
+            if (null == args)
+            {
+                throw new ArgumentNullException("args");
+            }
+
+            if (args.Help)
+            {
+                return;
+            }
+
+            if (args.Info)
+            {
+                Console.WriteLine(Environment.CurrentDirectory);
+                foreach (var project in ProjectCollection.Load(Environment.CurrentDirectory))
+                {
+                    Console.WriteLine(project.CSharp.FullName);
+                }
+            }
+
+            Console.Read();
+        }
+    }
+}
