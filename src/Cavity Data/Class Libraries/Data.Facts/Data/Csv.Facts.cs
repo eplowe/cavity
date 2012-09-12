3c53d8b419135d251d7f8bb56efd1cc21a64c23f
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2012-09-12 17:56:22 +0000

    Removed Csv.Line(...) overload.

--- src/Cavity Data/Class Libraries/Data.Facts/Data/Csv.Facts.cs
+++ src/Cavity Data/Class Libraries/Data.Facts/Data/Csv.Facts.cs
@@ -142,12 +142,6 @@
         }
 
         [Fact]
-        public void op_Line_KeyStringDictionaryNull_string()
-        {
-            Assert.Throws<ArgumentNullException>(() => Csv.Line(null, "A,C"));
-        }
-
-        [Fact]
         public void op_Line_KeyStringDictionary_IListOfString()
         {
             var obj = new KeyStringDictionary
@@ -179,40 +173,6 @@
         public void op_Line_KeyStringDictionary_IListOfString_whenColumnsNotFound()
         {
             Assert.Throws<KeyNotFoundException>(() => Csv.Line(new KeyStringDictionary(), "A,B".Split(',').ToList()));
-        }
-
-        [Fact]
-        public void op_Line_KeyStringDictionary_string()
-        {
-            var obj = new KeyStringDictionary
-                          {
-                              new KeyStringPair("A", "123"),
-                              new KeyStringPair("B", "ignore"),
-                              new KeyStringPair("C", "left,right")
-                          };
-
-            const string expected = "123,\"left,right\"";
-            var actual = Csv.Line(obj, "A|C");
-
-            Assert.Equal(expected, actual);
-        }
-
-        [Fact]
-        public void op_Line_KeyStringDictionary_stringEmpty()
-        {
-            Assert.Throws<ArgumentOutOfRangeException>(() => Csv.Line(new KeyStringDictionary(), string.Empty));
-        }
-
-        [Fact]
-        public void op_Line_KeyStringDictionary_stringNull()
-        {
-            Assert.Throws<ArgumentNullException>(() => Csv.Line(new KeyStringDictionary(), null as string));
-        }
-
-        [Fact]
-        public void op_Line_KeyStringDictionary_string_whenColumnsNotFound()
-        {
-            Assert.Throws<KeyNotFoundException>(() => Csv.Line(new KeyStringDictionary(), "A"));
         }
 
         [Fact]
