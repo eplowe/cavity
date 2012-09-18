97add49a97cb345a83076a36a9162dd9e99535d8
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2012-09-18 09:18:03 +0000

    Created CSV and TSV checker applications.

---
+++ src/Cavity Utilities/Console Applications/TSV/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+
+[assembly: CLSCompliant(true)]
+[assembly: AssemblyDefaultAlias("TSV.exe")]
+[assembly: AssemblyTitle("TSV.exe")]
+
+#if (DEBUG)
+
+[assembly: AssemblyDescription("Cavity : TSV Console Application (Debug)")]
+
+#else
+
+[assembly: AssemblyDescription("Cavity : TSV Console Application (Release)")]
+
+#endif
+
+[assembly: SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", MessageId = "TSV", Justification = "This casing is correct.")]
