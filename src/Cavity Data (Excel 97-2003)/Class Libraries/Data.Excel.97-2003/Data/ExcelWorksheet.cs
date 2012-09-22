e43c74c03a8b792579881e461482f07ab371bd5f
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2012-09-22 21:14:00 +0000

    Working on Excel 97-2003; tweaking nuget packages.

---
+++ src/Cavity Data (Excel 97-2003)/Class Libraries/Data.Excel.97-2003/Data/ExcelWorksheet.cs
@@ -0,0 +1,97 @@
+﻿namespace Cavity.Data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
+    using System.IO;
+
+    using Microsoft.Office.Interop.Excel;
+
+    [SuppressMessage("Microsoft.Naming", "CA1710:IdentifiersShouldHaveCorrectSuffix", Justification = "This is not a collection.")]
+    public sealed class ExcelWorksheet : DataSheet
+    {
+        public ExcelWorksheet(string path)
+            : this(new FileInfo(path))
+        {
+        }
+
+        public ExcelWorksheet(FileInfo info)
+        {
+            if (null == info)
+            {
+                throw new ArgumentNullException("info");
+            }
+
+            if (!info.Exists)
+            {
+                throw new FileNotFoundException(info.FullName);
+            }
+
+            Info = info;
+        }
+
+        public FileInfo Info { get; private set; }
+
+        protected override IEnumerator<T> GetEnumerator<T>()
+        {
+            Info.Refresh();
+            if (!Info.Exists)
+            {
+                yield break;
+            }
+
+            Application instance = null;
+            try
+            {
+                instance = new Application();
+                var workbook = instance.Workbooks.Open(Info.FullName, 0, true, 5, string.Empty, string.Empty, true, XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
+                _Worksheet worksheet = null;
+                foreach (_Worksheet sheet in workbook.Sheets)
+                {
+                    if (string.Equals(sheet.Name, Title, StringComparison.Ordinal))
+                    {
+                        worksheet = sheet;
+                        break;
+                    }
+                }
+
+                if (null == worksheet)
+                {
+                    yield break;
+                }
+
+                var range = worksheet.UsedRange;
+                var columns = new List<string>();
+                for (var i = 1; i < range.Columns.Count + 1; i++)
+                {
+                    var cell = (Range)range.Cells[1, i];
+                    columns.Add((string)cell.Text);
+                    
+                    //// columns.Add((string)range.Cells[1, i].Value);
+                }
+
+                for (var i = 2; i < range.Rows.Count + 1; i++)
+                {
+                    var entry = Activator.CreateInstance<T>();
+                    for (var j = 0; j < columns.Count; j++)
+                    {
+                        var cell = (Range)range.Cells[i, j + 1];
+                        entry.Add(columns[j], (string)cell.Text);
+
+                        ////var value = range.Cells[i, j + 1].Value ?? string.Empty;
+                        ////entry.Add(columns[j], value.ToString());
+                    }
+
+                    yield return entry;
+                }
+            }
+            finally
+            {
+                if (null != instance)
+                {
+                    instance.Quit();
+                }
+            }
+        }
+    }
+}
---
+++ src/Cavity Data (Excel 97-2003)/Class Libraries/Data.Excel.97-2003/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+
+[assembly: CLSCompliant(false)]
+[assembly: AssemblyDefaultAlias("Cavity.Data.Excel.97-2003.dll")]
+[assembly: AssemblyTitle("Cavity.Data.Excel.97-2003.dll")]
+
+#if (DEBUG)
+
+[assembly: AssemblyDescription("Cavity : Excel 97-2003 Data Library (Debug)")]
+
+#else
+
+[assembly: AssemblyDescription("Cavity : Excel 97-2003 Data Library (Release)")]
+
+#endif
+
+[assembly: SuppressMessage("Microsoft.Design", "CA1020:AvoidNamespacesWithFewTypes", Scope = "namespace", Target = "Cavity")]
