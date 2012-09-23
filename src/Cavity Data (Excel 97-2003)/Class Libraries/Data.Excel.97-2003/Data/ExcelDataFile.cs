519da1fd2936ce5de89d5fa5fb632e7b543a8149
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2012-09-23 00:39:36 +0000

    Implemented Excel 97-2003 nuget package.

---
+++ src/Cavity Data (Excel 97-2003)/Class Libraries/Data.Excel.97-2003/Data/ExcelDataFile.cs
@@ -0,0 +1,44 @@
+﻿namespace Cavity.Data
+{
+    using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
+    using System.IO;
+
+    using Microsoft.Office.Interop.Excel;
+
+    [SuppressMessage("Microsoft.Naming", "CA1710:IdentifiersShouldHaveCorrectSuffix", Justification = "This is not a collection.")]
+    public class ExcelDataFile : DataFile
+    {
+        public ExcelDataFile(FileInfo info)
+            : base(info)
+        {
+        }
+
+        public override IEnumerator<IDataSheet> GetEnumerator()
+        {
+            Info.Refresh();
+            if (!Info.Exists)
+            {
+                yield break;
+            }
+
+            Application instance = null;
+            try
+            {
+                instance = new Application();
+                var workbook = instance.Workbooks.Open(Info.FullName, 0, true, 5, string.Empty, string.Empty, true, XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
+                foreach (_Worksheet sheet in workbook.Sheets)
+                {
+                    yield return new ExcelDataSheet(Info, sheet);
+                }
+            }
+            finally
+            {
+                if (null != instance)
+                {
+                    instance.Quit();
+                }
+            }
+        }
+    }
+}
