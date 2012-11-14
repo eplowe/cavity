5e009f189878782e1c69f893e1052a8ae4efbe47
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2012-11-14 23:06:21 +0000

    Implemented IList<T>.RemoveLast(int)

--- trunk/src/Cavity Core/Version.cs
+++ trunk/src/Cavity Core/Version.cs
@@ -15,7 +15,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.916")]
-[assembly: AssemblyInformationalVersion("2.0.0.916")]
+[assembly: AssemblyFileVersion("2.0.0.917")]
+[assembly: AssemblyInformationalVersion("2.0.0.917")]
 
 
