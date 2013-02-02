d2f307dd00cdd48285b3077c1d37d2cdb92e6f40
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2013-02-02 21:03:10 +0000

    Created LINES console app.

---
+++ src/Cavity Utilities/Console Applications/LINES/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+
+[assembly: CLSCompliant(true)]
+[assembly: AssemblyDefaultAlias("LINES.exe")]
+[assembly: AssemblyTitle("LINES.exe")]
+
+#if (DEBUG)
+
+[assembly: AssemblyDescription("Cavity : LINES Console Application (Debug)")]
+
+#else
+
+[assembly: AssemblyDescription("Cavity : LINES Console Application (Release)")]
+
+#endif
+
+[assembly: SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", MessageId = "LINES", Justification = "This casing is correct.")]
