0787220a00feb9bc95c8f30a99febf41500197a2
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2013-02-02 21:03:10 +0000

    Created LINES console app.

---
+++ trunk/src/Cavity Utilities/Console Applications/LINES/Arguments.cs
@@ -0,0 +1,88 @@
+﻿namespace Cavity
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    using Cavity.IO;
+    using Cavity.Properties;
+
+    public sealed class Arguments
+    {
+        public Arguments()
+        {
+            Specs = new List<FileSpec>();
+        }
+
+        public bool Help { get; private set; }
+
+        public IList<FileSpec> Specs { get; private set; }
+
+        public static Arguments Load(string[] args)
+        {
+            if (null == args || 0 == args.Length)
+            {
+                return new Arguments
+                {
+                    Help = true
+                };
+            }
+
+            var result = new Arguments();
+            foreach (var arg in args)
+            {
+                switch (arg.ToUpperInvariant())
+                {
+                    case "/?":
+                    case "/HELP":
+                    case "-HELP":
+                        result.Help = true;
+                        break;
+
+                    default:
+                        result.Specs.Add(new FileSpec(arg));
+                        break;
+                }
+            }
+
+            return result;
+        }
+
+        public void Process()
+        {
+            if (Help)
+            {
+                Console.WriteLine(Resources.UsageHelp);
+                Console.WriteLine(string.Empty);
+                return;
+            }
+
+            foreach (var file in Specs.SelectMany(spec => spec))
+            {
+                Process(file);
+            }
+
+            Console.WriteLine(new string('=', 30));
+        }
+
+        private static void Process(FileSystemInfo file)
+        {
+            Console.WriteLine(Resources.FileInfo, file.FullName);
+            long count = 0;
+            using (var stream = File.Open(file.FullName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            {
+                using (var reader = new StreamReader(stream))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        reader.ReadLine();
+                        count++;
+                    }
+                }
+            }
+
+            Console.WriteLine(Resources.LineCount, count);
+        }
+    }
+}
