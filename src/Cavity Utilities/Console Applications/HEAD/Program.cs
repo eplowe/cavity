a57aafe24b7e9a14928c7582bdf534c2584819ca
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2013-04-21 11:02:14 +0000

    Implemented HEAD console app.

---
+++ src/Cavity Utilities/Console Applications/HEAD/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Cavity
+{
+    using System;
+
+    using Cavity.Properties;
+
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Arguments.Load(args).Process();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(Resources.ExceptionFormat, exception.GetType().Name, exception.Message);
+            }
+        }
+
+        public static void OnUnhandledException(object sender,
+                                                UnhandledExceptionEventArgs e)
+        {
+            if (null == e)
+            {
+                return;
+            }
+
+            Console.WriteLine(e.ExceptionObject);
+        }
+    }
+}
---
+++ src/Cavity Utilities/Console Applications/HEAD/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+
+[assembly: CLSCompliant(true)]
+[assembly: AssemblyDefaultAlias("HEAD.exe")]
+[assembly: AssemblyTitle("HEAD.exe")]
+
+#if (DEBUG)
+
+[assembly: AssemblyDescription("Cavity : HEAD Console Application (Debug)")]
+
+#else
+
+[assembly: AssemblyDescription("Cavity : HEAD Console Application (Release)")]
+
+#endif
+
+[assembly: SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", MessageId = "HEAD", Justification = "This casing is correct.")]
