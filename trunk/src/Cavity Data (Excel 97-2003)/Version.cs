376f98969b46bf519093900ca22bc51f8da0a7e6
alan.dean@gmail.com <alan.dean@gmail.com@67bafd89-7b61-e115-2f88-658d790e9269>
2013-09-14 17:29:08 +0000

    Updated NuGet packages.

--- trunk/src/Cavity Data (Excel 97-2003)/Version.cs
+++ trunk/src/Cavity Data (Excel 97-2003)/Version.cs
@@ -15,7 +15,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.942")]
-[assembly: AssemblyInformationalVersion("2.0.0.942")]
+[assembly: AssemblyFileVersion("2.0.0.943")]
+[assembly: AssemblyInformationalVersion("2.0.0.943")]
 
 
