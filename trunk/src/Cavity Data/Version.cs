e88b4bef649f1ab6f84eda8121aca9e135a77a58
alan.dean@gmail.com <alan.dean@gmail.com@67bafd89-7b61-e115-2f88-658d790e9269>
2013-09-15 03:22:22 +0000

    Working on fixing NuGet bundling.

--- trunk/src/Cavity Data/Version.cs
+++ trunk/src/Cavity Data/Version.cs
@@ -15,7 +15,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.946")]
-[assembly: AssemblyInformationalVersion("2.0.0.946")]
+[assembly: AssemblyFileVersion("2.0.0.947")]
+[assembly: AssemblyInformationalVersion("2.0.0.947")]
 
 
