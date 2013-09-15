f46dad17fccd1800e425e647fe392f6f77997e19
alan.dean@gmail.com <alan.dean@gmail.com@67bafd89-7b61-e115-2f88-658d790e9269>
2013-09-15 01:40:32 +0000

    BritishTelephone... DataFileCreation TempCsvFile

--- trunk/src/Cavity Domain (Royal Mail)/Version.cs
+++ trunk/src/Cavity Domain (Royal Mail)/Version.cs
@@ -15,7 +15,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.944")]
-[assembly: AssemblyInformationalVersion("2.0.0.944")]
+[assembly: AssemblyFileVersion("2.0.0.945")]
+[assembly: AssemblyInformationalVersion("2.0.0.945")]
 
 
