4cb82b2dbde483fb2eaa58b54e5a5c49054d1449
alan.dean <alan.dean@gmail.com>
2013-09-25 21:22:10 +0000

    Fixed issue with mobile numbers being declared invalid.

--- src/Cavity Domain/Version.cs
+++ src/Cavity Domain/Version.cs
@@ -15,7 +15,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.949")]
-[assembly: AssemblyInformationalVersion("2.0.0.949")]
+[assembly: AssemblyFileVersion("2.0.0.956")]
+[assembly: AssemblyInformationalVersion("2.0.0.956")]
 
 
