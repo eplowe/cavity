b3c469fc90f0b3f7df53dc5f099a371b0280f22c
alan.dean <alan.dean@67bafd89-7b61-e115-2f88-658d790e9269>
2014-11-25 10:01:40 +0000

    Upgraded Cavity Core packages.

--- trunk/src/Cavity Domain (Royal Mail)/Version.cs
+++ trunk/src/Cavity Domain (Royal Mail)/Version.cs
@@ -15,7 +15,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.969")]
-[assembly: AssemblyInformationalVersion("2.0.0.969")]
+[assembly: AssemblyFileVersion("2.0.0.970")]
+[assembly: AssemblyInformationalVersion("2.0.0.970")]
 
 
